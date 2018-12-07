726776f91eba155c0d049a3b83341f2e38075767
Hechix <jatonidonet@hotmail.com>
2018-12-07 10:12:35 +0100

    feat(#1): Soporte para carpeta imagenes, llenar directorios vacios con .gitkeep

--- build/build.sh
+++ build/build.sh
@@ -5,15 +5,18 @@ printf "\tEliminando dist/\n"
 rm dist -rf
 printf "\tCreando árbol vacio\n"
 mkdir dist
-mkdir dist/app dist/config dist/core dist/css dist/js dist/modelo dist/vista
+mkdir dist/css dist/js dist/imagenes
 printf "\tCopiando archivos servidor\n"
 rsync -a src/ dist/ --exclude vista/cliente
+cp src/vista/cliente/imagenes/* dist/imagenes 
 printf "\tCompilando SASS y TypeScript\n"
 sass src/vista/cliente/scss/estilo.scss dist/css/estilo.css
 printf "\t\tSASS compilado.\n"
 tsc src/vista/cliente/ts/* --outDir dist/js/
 printf "\t\tTypeScript compilado.\n"
-
+for dire in $(find dist -empty -type d); do
+    touch $dire/.gitkeep
+    echo Creado $dire/.gitkeep
+done
 du -h dist
 printf "\nDesplegado satisfactoriamente.\n"
-#tree
