3f93f22b1404898c71386534b35a9b52a8f5a194
Hechix <jatonidonet@hotmail.com>
2018-12-07 10:52:33 +0100

    feat(#3): Modificacion pertinente

--- build/build.sh
+++ build/build.sh
@@ -7,16 +7,13 @@ printf "\tCreando árbol vacio\n"
 mkdir dist
 mkdir dist/css dist/js dist/imagenes
 printf "\tCopiando archivos servidor\n"
-rsync -a src/ dist/ --exclude vista/cliente
+cp src/* dist/ -r
+rm dist/vista/cliente -r
 cp src/vista/cliente/imagenes/* dist/imagenes 
 printf "\tCompilando SASS y TypeScript\n"
 sass src/vista/cliente/scss/estilo.scss dist/css/estilo.css
 printf "\t\tSASS compilado.\n"
 tsc src/vista/cliente/ts/* --outDir dist/js/
 printf "\t\tTypeScript compilado.\n"
-for dire in $(find dist -empty -type d); do
-    touch $dire/.gitkeep
-    echo Creado $dire/.gitkeep
-done
 du -h dist
 printf "\nDesplegado satisfactoriamente.\n"
