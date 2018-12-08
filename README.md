f47592d645427a0319e99759686c5655f2ed26fa
yuriyfpmislata <yuriy997+clase@gmail.com>
2018-12-08 18:07:31 +0100

    feat(#4): Mejoras en el proceso de construcción

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # WeekFood
 Proyecto WeekFood para 2º curso de Desarrollo de Aplicaciones Web
+
+## Instalar dependencias
+```
+npm install
+```
+
+## Construir dist/
+_(ejecutar desde Git Bash o bash nativo)_
+```
+npm run build
+```
--- build/build.sh
+++ build/build.sh
@@ -1,19 +1,31 @@
-printf "\n- - - - - - - - - - - - - - - \n"
+#!/bin/bash
+
+set -e # salir si cualquier comando no sale con 0 [https://stackoverflow.com/a/2871034/3499595]
+shopt -s globstar # activar el glob ** para poder recorrer directorios recursivamente [https://unix.stackexchange.com/a/49917/295092]
+
 date +"%T"
 printf "Construyendo...\n"
-printf "\tEliminando dist/\n"
-rm dist -rf
-printf "\tCreando árbol vacio\n"
-mkdir dist
-mkdir dist/css dist/js dist/imagenes
-printf "\tCopiando archivos servidor\n"
-cp src/* dist/ -r
-rm dist/vista/cliente -r
-cp src/vista/cliente/imagenes/* dist/imagenes 
-printf "\tCompilando SASS y TypeScript\n"
-sass src/vista/cliente/scss/estilo.scss dist/css/estilo.css
-printf "\t\tSASS compilado.\n"
-tsc src/vista/cliente/ts/* --outDir dist/js/
-printf "\t\tTypeScript compilado.\n"
+
+printf "\t> Eliminando dist/\n"
+    rm -rf dist
+
+printf "\t> Creando directorio dist/\n"
+    mkdir dist
+
+printf "\t> Copiando archivos\n"
+    cp -r src/* dist/
+	# borrar directorios copiados que se tienen que construir en vez de copiar
+    rm -r dist/vista/cliente/{scss,ts}
+
+printf "\t> Compilando Sass\n"
+	node-sass --quiet --output-style expanded --source-map true src/vista/cliente/scss/estilo.scss --output dist/css
+	printf "\t\t> Sass compilado.\n"
+
+printf "\t> Compilando TypeScript\n"
+	# ** recorre directorios recursivamente
+	tsc src/vista/cliente/ts/**/*.ts --out dist/js/script.js
+	printf "\t\t> TypeScript compilado.\n\n"
+
 du -h dist
-printf "\nDesplegado satisfactoriamente.\n"
+
+printf "\nConstruido satisfactoriamente.\n"
