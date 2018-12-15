19610fefc211c6585526abab4ff518860c0b4552
Hechix <jatonidonet@hotmail.com>
2018-12-15 19:55:28 +0100

    Vale si que se puede exportar la creacion de base de datos

--- db/WeekFood.sql
+++ db/WeekFood.sql
@@ -16,6 +16,14 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Current Database: `WeekFood`
+--
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `WeekFood` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+
+USE `WeekFood`;
+
+--
 -- Table structure for table `Carrusel`
 --
 
@@ -96,4 +104,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-12-13 12:19:30
+-- Dump completed on 2018-12-15 19:52:59
