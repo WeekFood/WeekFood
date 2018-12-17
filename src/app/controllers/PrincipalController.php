64a2910c4b89127d6e92ce63293a292998de1866
Hechix <jatonidonet@hotmail.com>
2018-12-17 11:50:12 +0100

    intentando que /api responda independientemente de si tiene argumentos

--- src/app/controllers/PrincipalController.php
+++ src/app/controllers/PrincipalController.php
@@ -10,4 +10,17 @@ class PrincipalController extends core\MVC\Action
         $this->setView("api");
         $this->renderView();
     }
+    /* //Para no discriminar si viene con ?x o ?x=y, buscar forma de hacerlo automatico
+public function Api_param_valorAction()
+{
+//$this->params["parametro"] = $this->getParam("parametro");
+$this->setView("api"); //,$this->params);
+$this->renderView();
+}public function Api_paramAction()
+{
+//$this->params["parametro"] = $this->getParam("parametro");
+$this->setView("api"); //,$this->params);
+$this->renderView();
+}
+ */
 }
--- src/configs/routes.php
+++ src/configs/routes.php
@@ -13,6 +13,17 @@ return [
             "controller" => "principal",
             "action" => "api",
         ),
+        /* //Para no discriminar si viene con ?x o ?x=y, buscar forma de hacerlo automatico
+    "API parametro con valor" => array(
+    "route" => "api?:parametro=:valor",
+    "controller" => "principal",
+    "action" => "api_param_valor",
+    ),
+    "API parametro sin valor" => array(
+    "route" => "api?:parametro",
+    "controller" => "principal",
+    "action" => "api_param",
+    ),*/
     ],
     "error" => "error.php",
 ];
