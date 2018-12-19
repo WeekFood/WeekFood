bc6237d5a4563c88b649d93349f305a7c0bd0146
jlsanchezdelgado95 <kokekui17@gmail.com>
2018-12-19 12:35:23 +0100

    feat(#21) : Montar Carrusel con GlideJS pr(#47)
    * json integrado
    
    * actualizar Json
    
    * Borrado de estilos Carusel

--- src/app/views/cliente/js/carusel.js
+++ src/app/views/cliente/js/carusel.js
@@ -1,1 +1,28 @@
-// new Glide... aqui
+function generarCarrusel(claseContenedor) {
+    var contenido = "";
+    var contenedor = document.getElementsByClassName(claseContenedor)[0];
+    $.getJSON("/api/carrusel", (datos) => {
+        if (datos["error"] == false) {
+            contenido += "<div class='glide'>";
+
+            contenido += "<div class='glide__track' data-glide-el='track'>";
+
+            contenido += "<div class='glide__slides'>";
+            
+            for (let index = 0; index < datos["data"].length; index++) {
+                contenido += "<img class='glide__slide' src='imagenes/productos/" + datos["data"][index]["foto"] + "'>";
+            }
+            contenido += "</div></div></div>";
+            contenedor.innerHTML = contenido;
+            new Glide('.glide', {
+                type: 'carousel',
+                autoplay: 1,
+                animationDuration: 5000,
+                hoverpause: false,
+                perView: 3,
+                gap: 0,
+                animationTimingFunc: 'linear'
+            }).mount()
+        }
+    })
+}
--- src/app/views/index.php
+++ src/app/views/index.php
@@ -48,6 +48,8 @@
     </div>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="js/bundle.js"></script>
+    <script src="js/libs/glide-3.2.4/glide.min.js"></script>
+    <script src="js/carusel.js"></script>
 </body>
 
 </html>
