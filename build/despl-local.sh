9450aceb9ce98ebbb5248f3a9e0bfaa65781eb6f
Hechix <jatonidonet@hotmail.com>
2018-12-20 10:09:52 +0100

    feat(#?) : Añadidos scripts de npm para deploy, y combinacion de ambos

--- build/despl-local.sh
+++ build/despl-local.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
-if [ "$EUID" -ne 0 ]; then
-    ./build/build.sh
-    echo "Copiando a /var/www/html..."
-    sudo rm /var/www/html -rf
-    sudo mkdir /var/www/html
-    sudo cp ./build/config/htaccess /var/www/html/.htaccess
+if [ "$EUID" -eq 0 ]; then
+    echo "Copiando dist/ a /var/www/html..."
+    sudo rm /var/www/html -rf && 
+    sudo mkdir /var/www/html &&
+    sudo cp ./build/config/htaccess /var/www/html/.htaccess &&
     sudo cp ./dist/* /var/www/html/ -r
     echo "Copiado correctamente."
 else
-    echo "No me ejecutes como ROOT."
+    echo "Ejecutame como root."
     exit 1
 fi
