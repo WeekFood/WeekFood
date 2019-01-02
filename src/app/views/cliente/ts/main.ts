449f24bad50a20757a500d959837351919dfed00
Hechix <jatonidonet@hotmail.com>
2019-01-02 03:10:56 +0100

    Arreglado minibug en main.ts cambiado scss solo de pagina principal

--- src/app/views/cliente/ts/main.ts
+++ src/app/views/cliente/ts/main.ts
@@ -33,7 +33,7 @@ var vistas = {
     portada: {
         menu: "/api/menu/portada",
         scripts: ["js/libs/glide-3.2.4/glide.min.js"],
-        procesado: (datos) => { $(".l-distribucion__principal").html("<div class='c-principal'><div class='js-carrusel'></div><h1>¡Bienvenido a <span class='c-logo__parte'>Week</span><span class='c-logo__parte c-logo__parte--alterna'>Food</span>!</h1>Somos una empresa ficticia, ofrecemos gran variedad de platos preparados para entrega en el hogar, el trabajo, o la escuela.<br>Con <span class='c-logo__parte'>Week</span><span class='c-logo__parte c-logo__parte--alterna'>Food</span> nunca mas tendrás que preocuparte de que vas a comer hoy.<div class='c-principal__texto'></div></div>") },
+        procesado: (datos) => { $(".l-distribucion__principal").html("<div class='c-principal'><div class='js-carrusel'></div><div class='c-principal__texto'><h1>¡Bienvenido a <span class='c-logo__parte'>Week</span><span class='c-logo__parte c-logo__parte--alterna'>Food</span>!</h1>Somos una empresa ficticia, ofrecemos gran variedad de platos preparados para entrega en el hogar, el trabajo, o la escuela.<br>Con <span class='c-logo__parte'>Week</span><span class='c-logo__parte c-logo__parte--alterna'>Food</span> nunca mas tendrás que preocuparte de que vas a comer hoy.</div></div>") },
         postProcesado: () => { generarCarrusel("js-carrusel") },
     },
     productos: {
