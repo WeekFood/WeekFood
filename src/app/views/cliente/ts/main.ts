c9f29102d20cf11d7a6c2bacfbd0188d514eef4b
Hechix <jatonidonet@hotmail.com>
2019-01-02 20:17:55 +0100

    feat(#61) : Las vistas pasadas al cliente, cambio de landing.ts a main.ts y separacion de vistas en script independiente. PR(#77)
    Al hacer el rework tambien soluciona #66.

---
+++ src/app/views/cliente/ts/main.ts
@@ -0,0 +1,52 @@
+var vistas = {
+    error: vista_Error,
+    portada: vista_Portada,
+    productos: vista_Productos
+}
+
+function cargarVista(vistaACargar) {
+    cargarVistaAPunto(vistaACargar, ".l-distribucion__principal")
+}
+function cargarVistaAPunto(vistaACargar : string, puntoMontaje : string) {
+    if (vistas.hasOwnProperty(vistaACargar)) {
+        if (typeof (puntoMontaje) == "string") {
+            desplegarPanelCarga()
+            $.when(vistas[vistaACargar](puntoMontaje)).then(eliminarPanelCarga);
+        } else {
+            console.error("Error intentando cargar " + vistaACargar + " : El punto de montaje no es valido.")
+        }
+
+    } else {
+        cargarVistaAPunto('error', puntoMontaje)
+    }
+}
+
+function montarMenu(url, vista) {
+    return $.getJSON(url).then(
+        (datos) => {
+            var menu = ""
+            datos["data"].forEach(item => {
+                menu += "<li class='c-menu__item"
+                if (item["direccion"] == vista) {
+                    menu += " c-menu__item--destacado"
+                }
+                menu += "' onclick='cargarVista(\"" + item["direccion"] + "\")'>" + item["valor"] + "</li>"
+            });
+            $(".js-menu__lista").html(menu)
+            return true
+        }
+    )
+}
+
+function desplegarPanelCarga() {
+    // Todo Toni : Pre-Sprint 2 ó Sprint 2
+}
+function eliminarPanelCarga() {
+    // Todo Toni : Pre-Sprint 2 ó Sprint 2
+}
+
+$('.js-boton-menu').on('click', () => {
+    $('.js-menu').toggleClass('c-menu--oculto');
+});
+
+cargarVista("portada")
