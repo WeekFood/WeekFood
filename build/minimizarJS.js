69ba4b87d5ea3a974d267487793a5e618b51c72d
Hechix <jatonidonet@hotmail.com>
2019-01-05 21:46:33 +0100

    Codec cambiado a terser y optimizado

--- build/minimizarJS.js
+++ build/minimizarJS.js
@@ -1,7 +1,7 @@
-var compressor = require('node-minify');
-
-compressor.minify({
-    compressor: 'gcc',
+var mini = require('@node-minify/core');
+var ter = require('@node-minify/terser')
+mini({
+    compressor: ter,
     input: ['src/app/views/cliente/js/*.js','src/app/views/cliente/js/*/*.js'],
     output: 'dist/js/WeekFood.js',
     callback: function (err, min) { }
