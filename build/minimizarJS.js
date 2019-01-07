4502d1845d79b37e17631a0a46416f21279dc694
yuriy <36030998+yuriyfpmislata@users.noreply.github.com>
2019-01-07 08:29:32 +0100

    feat(#64): Mejorar el flujo de desarrollo (#89)
    * vs code: settings para usar git bash como terminal en windows
    
    * scripts npm: añadido "servidor" para usar el servidor de desarrollo de php
    
    * gitignore: ignorar el estilo.css buildeado para desarrollo
    
    * scripts npm: compilar y watch sass para desarrollo
    
    * minimizarJS: actualizado para aceptar ruta de output, quitado callback sin usar
    
    * scripts npm: miniJS para desarrollo
    
    * gitignore: ignorar WeekFood.js para desarrollo
    
    * scripts npm: añadido onchange para watch js y ejecutar dev:miniJS
    
    * scripts npm: creado browser-sync, ignorando scss y js, que se tienen que compilar con otros scripts antes de ser detectados que se han cambiado
    
    * añadido npm-run-all para poder ejecutar cosas en paralelo
    
    * scripts npm: BS: ignorar todos los js menos WeekFood, que cambiará por el watcher miniJS & script cliente
    
    * minimizarJS: ignorar WeekFood.js para dessarrollo
    
    * watch sass: --quiet
    
    * package-lock: testeado desde ubuntu se actualiza con optional en algunas entradas
    
    * package-lock: pero el npm de windows dice que no le hace falta... al parecer hay un issue abierto npm/npm#17722
    
    * scripts npm: cambiado nombre servidor -> server

--- build/minimizarJS.js
+++ build/minimizarJS.js
@@ -1,8 +1,8 @@
 var mini = require('@node-minify/core');
-var ter = require('@node-minify/terser')
+var ter = require('@node-minify/terser');
+
 mini({
     compressor: ter,
-    input: ['src/app/views/cliente/js/*.js','src/app/views/cliente/js/*/*.js'],
-    output: 'dist/js/WeekFood.js',
-    callback: function (err, min) { }
+    input: ['src/app/views/cliente/js/**/!(WeekFood).js'],
+    output: process.argv[2] || 'dist/js/WeekFood.js'
 });
