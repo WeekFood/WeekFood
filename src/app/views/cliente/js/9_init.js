f9546a1e500f49f4799d639a78bca3adfd6fe91f
Hechix <jatonidonet@hotmail.com>
2019-01-08 19:40:03 +0100

    Separado y funciona, necesita los numeros para la declaracion correcta

---
+++ src/app/views/cliente/js/9_init.js
@@ -0,0 +1,28 @@
+$(".js-menu").on('webkitAnimationEnd oanimationEnd msAnimationEnd animationend', () => {
+    if($('.js-menu').hasClass('c-menu--oculto')){
+        for (var x= 0;x < $(".js-menu__lista")[0].childElementCount;x++ ){
+            $($(".js-menu__lista").children()[x]).addClass("c-menu__item--oculto").removeClass("c-menu__item--descubrir")
+        }
+    }
+})
+
+$('.js-boton-menu').on('click', () => {
+    $(".js-menu").removeClass("c-menu--descubrir");
+
+    if($('.js-menu').hasClass('c-menu--oculto')){    
+        $(".js-menu").removeClass("c-menu--oculto c-menu--ocultar").addClass("c-menu--descubrir")
+        x = 0
+        var modificarListado = setInterval(()=>{
+            if (x < $(".js-menu__lista")[0].childElementCount){
+                $($(".js-menu__lista").children()[x]).removeClass("c-menu__item--oculto").addClass("c-menu__item--descubrir")
+                x++
+            }else{
+                clearInterval(modificarListado);
+            }
+        },175)
+        
+    } else {
+        $(".js-menu").addClass("c-menu--ocultar c-menu--oculto")
+    }
+});
+cargarVista("portada")
