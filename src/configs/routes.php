3c07fa5ee0f0ba6b8daf41855d8952a22804c20c
yuriyfpmislata <yuriy997+clase@gmail.com>
2019-01-08 21:44:05 +0100

    routes portado & ClienteResource

--- src/configs/routes.php
+++ src/configs/routes.php
@@ -1,44 +1,31 @@
 <?php
 
-
-return array(	
-	"get" => array(
-		"Equipos" => array(
-			"route" => "equipos",
-			"resource" => "equipos",
-			"action" => "getAll"
-		),
-
-		"Equipo" => array(
-			"route" => "equipos/:equipo",
-			"resource" => "equipos",
-			"action" => "getById"
-		),
-
-		"Jugadores" => array(
-			"route" => "jugadores",
-			"resource" => "jugadores",
-			"action" => "getAll"
-		),
-
-		"jugador" => array(
-			"route" => "jugadores/:jugador",
-			"resource" => "jugadores",
-			"action" => "getById"
-		), 
-
-		"JugadoresEquipo" => array(
-			"route" => "equipos/:equipo/jugadores",
-			"resource" => "jugadores",
-			"action" => "getByTeam"
-		),
-
-		"PartidosEquipo" => array(
-			"route" => "equipos/:equipo/partidos",
-			"resource" => "partidos"
-		)
-	)
-
-);
-
-
+return [
+    "get" => [
+        "/" => array(
+            "route" => "/",
+            "resource" => "cliente",
+            "action" => "index",
+        ),
+        "API" => array(
+            "route" => "api",
+            "resource" => "Api",
+            "action" => "Api",
+        ),
+        "API, carrusel" => array(
+            "route" => "api/carrusel",
+            "resource" => "carrusel",
+            "action" => "carrusel",
+        ),
+        "API, menu" => array(
+            "route" => "api/menu/:pagina",
+            "resource" => "menu",
+            "action" => "menu",
+        ),
+        "API, productos" => array(
+            "route" => "api/productos",
+            "resource" => "productos",
+            "action" => "productos",
+        ),
+    ]
+];
