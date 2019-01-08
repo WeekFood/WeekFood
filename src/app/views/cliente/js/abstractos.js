a4b46e10c4946325fcb18313fc38ce1b08b6dbb8
Hechix <jatonidonet@hotmail.com>
2019-01-08 22:23:06 +0100

    feat(#99) : Separacion de main.js en varios archivos para facilitar el desarrollo. PR (#100)

---
+++ src/app/views/cliente/js/abstractos.js
@@ -0,0 +1,5 @@
+var vistas = {
+    error: vista_Error,
+    portada: vista_Portada,
+    productos: vista_Productos
+}
--- src/app/views/cliente/js/funciones.js
+++ src/app/views/cliente/js/funciones.js
@@ -1,9 +1,3 @@
-var vistas = {
-    error: vista_Error,
-    portada: vista_Portada,
-    productos: vista_Productos
-}
-
 function cargarVista(vistaACargar) {
     cargarVistaAPunto(vistaACargar, ".l-distribucion__principal")
 }
@@ -47,31 +41,3 @@ function desplegarPanelCarga() {
 function eliminarPanelCarga() {
     // Todo Toni : Pre-Sprint 2 ó Sprint 2
 }
-$(".js-menu").on('webkitAnimationEnd oanimationEnd msAnimationEnd animationend', () => {
-    if($('.js-menu').hasClass('c-menu--oculto')){
-        for (var x= 0;x < $(".js-menu__lista")[0].childElementCount;x++ ){
-            $($(".js-menu__lista").children()[x]).addClass("c-menu__item--oculto").removeClass("c-menu__item--descubrir")
-        }
-    }
-})
-
-$('.js-boton-menu').on('click', () => {
-    $(".js-menu").removeClass("c-menu--descubrir");
-
-    if($('.js-menu').hasClass('c-menu--oculto')){    
-        $(".js-menu").removeClass("c-menu--oculto c-menu--ocultar").addClass("c-menu--descubrir")
-        x = 0
-        var modificarListado = setInterval(()=>{
-            if (x < $(".js-menu__lista")[0].childElementCount){
-                $($(".js-menu__lista").children()[x]).removeClass("c-menu__item--oculto").addClass("c-menu__item--descubrir")
-                x++
-            }else{
-                clearInterval(modificarListado);
-            }
-        },175)
-        
-    } else {
-        $(".js-menu").addClass("c-menu--ocultar c-menu--oculto")
-    }
-});
-cargarVista("portada")
