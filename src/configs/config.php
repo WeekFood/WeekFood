5107e0e7de14d9edd5573379e81f51163742874b
yuriyfpmislata <yuriy997+clase@gmail.com>
2019-01-08 21:38:53 +0100

    config.php portado

--- src/configs/config.php
+++ src/configs/config.php
@@ -1,24 +1,18 @@
 <?php
 
     define('DS', DIRECTORY_SEPARATOR);
-    define("ROOT", "/home/cesar/www/DWS/api");    
-    //define("ROOT2", "http://api.test");    
-    /*DEFINE ("VISTAS", ROOT . DS . "vistas");
-    DEFINE ("CSS", ROOT2 . DS . "vistas" . DS . "css");
-    DEFINE ("IMAGENES", ROOT2 . DS ."imagenes");
-    DEFINE ("FICHEROS", ROOT2 . DS ."ficheros");
-    DEFINE ("FICHEROS2", ROOT . DS ."ficheros");
-    DEFINE ("AVATARES", ROOT2 . DS ."avatares");
-    DEFINE ("AVATARES2", ROOT . DS ."avatares");
-    DEFINE ("NOMBREAVATAR", "avatar");
-    DEFINE ("EQUIPO", "Lakers");*/
+    define('ROOT', $_SERVER['DOCUMENT_ROOT']);
 
-    return array(
-        "site" => array(
-            "name" => "nba.com",
-            "title"  => "LAKERS",
-            "coreMVC" => ROOT . DS . "core" . DS . "MVC" . DS,
-            "configs" => ROOT . DS . "configs" . DS,
-            "resources" => ROOT . DS . "app" . DS . "resources" . DS, 
-        )
-    );
+    return [
+        'db' => [
+            'name' => 'WeekFood',
+            'host' => 'localhost',
+            'user' => 'WeekFood',
+            'password' => 'SQITBAxsMBPuQtaZ'
+        ],
+        'site' => [
+            'coreMVC' => ROOT . DS . 'core' . DS . 'MVC' . DS,
+            'configs' => ROOT . DS . 'configs' . DS,
+            'resources' => ROOT . DS . 'app' . DS . 'resources' . DS
+        ]
+    ];
