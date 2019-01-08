72c57089d093f7a6d1c926c28af76855f4f32d04
Hechix <jatonidonet@hotmail.com>
2019-01-08 19:30:20 +0100

    feat(#90) : Animacion despliegue menu movil. PR(#92)

--- src/app/views/cliente/js/main.js
+++ src/app/views/cliente/js/main.js
@@ -27,6 +27,9 @@ function montarMenu(url, vista) {
             var menu = ""
             datos["data"].forEach(item => {
                 menu += "<li class='c-menu__item"
+                if($('.js-menu').hasClass('c-menu--oculto')){
+                    menu += " c-menu__item--oculto"
+                }
                 if (item["direccion"] == vista) {
                     menu += " c-menu__item--destacado"
                 }
@@ -44,9 +47,31 @@ function desplegarPanelCarga() {
 function eliminarPanelCarga() {
     // Todo Toni : Pre-Sprint 2 ó Sprint 2
 }
+$(".js-menu").on('webkitAnimationEnd oanimationEnd msAnimationEnd animationend', () => {
+    if($('.js-menu').hasClass('c-menu--oculto')){
+        for (var x= 0;x < $(".js-menu__lista")[0].childElementCount;x++ ){
+            $($(".js-menu__lista").children()[x]).addClass("c-menu__item--oculto").removeClass("c-menu__item--descubrir")
+        }
+    }
+})
 
 $('.js-boton-menu').on('click', () => {
-    $('.js-menu').toggleClass('c-menu--oculto');
-});
+    $(".js-menu").removeClass("c-menu--descubrir");
 
+    if($('.js-menu').hasClass('c-menu--oculto')){    
+        $(".js-menu").removeClass("c-menu--oculto c-menu--ocultar").addClass("c-menu--descubrir")
+        x = 0
+        var modificarListado = setInterval(()=>{
+            if (x < $(".js-menu__lista")[0].childElementCount){
+                $($(".js-menu__lista").children()[x]).removeClass("c-menu__item--oculto").addClass("c-menu__item--descubrir")
+                x++
+            }else{
+                clearInterval(modificarListado);
+            }
+        },175)
+        
+    } else {
+        $(".js-menu").addClass("c-menu--ocultar c-menu--oculto")
+    }
+});
 cargarVista("portada")
