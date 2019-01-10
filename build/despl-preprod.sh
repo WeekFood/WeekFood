5bf24abee28c950d711d1f805ef2ecec712613d0
Hechix <jatonidonet@hotmail.com>
2019-01-10 11:35:17 +0100

    adaptadoa  dev

--- build/despl-preprod.sh
+++ build/despl-preprod.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-echo "Copiando dist/ a ../www"
-rm ../www -rf && 
-mkdir ../www &&
-cp ./build/config/htaccess ../www/.htaccess &&
-cp ./dist/* /../www -r
-mv ../www/configs/preprod_config.php ../www/configs/config.php
+echo "Copiando dist/ a ../../www"
+rm ../../www -rf && 
+mkdir ../../www &&
+cp ./build/config/htaccess ../../www/.htaccess &&
+cp ./dist/* ../../www -r
+mv ../../www/configs/preprod_config.php ../../www/configs/config.php
 echo "Copiado correctamente."
