54c553505ff3efbe34bdcea96395b7619c2148da
Hechix <jatonidonet@hotmail.com>
2019-01-10 11:48:26 +0100

    adaptado para que no de errores docker

--- build/despl-preprod.sh
+++ build/despl-preprod.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 echo "Copiando dist/ a ../../www"
-rm ../../www -rf && 
-mkdir ../../www &&
+rm ../../www/* -rf && 
 cp ./build/config/htaccess ../../www/.htaccess &&
 cp ./dist/* ../../www -r
 mv ../../www/configs/preprod_config.php ../../www/configs/config.php
