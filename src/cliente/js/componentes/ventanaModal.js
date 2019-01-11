5260fdfbb5970df87a7cbff3ea59a085456e3ff4
Hechix <jatonidonet@hotmail.com>
2019-01-11 10:05:29 +0100

    Cambios en la estructura, estilizado para movil, cambio de X

--- src/cliente/js/componentes/ventanaModal.js
+++ src/cliente/js/componentes/ventanaModal.js
@@ -6,12 +6,12 @@ function generarVentanaModal(tamaño, titulo, contenido) {
     }
     var modal = "<div class='c-ventana-modal js-ventana-modal'>"
     modal += "<div class='c-ventana-modal__interno c-ventana-modal__interno--" + tamaño + "'>"
-    modal += "<div class='c-ventana-modal__equis js-ventana-modal__equis'></div>"
+    modal += "<div class='c-ventana-modal__equis js-ventana-modal__cerrar'></div>"
     modal += "<div class='c-ventana-modal__titulo'>" + titulo + "</div>"
     modal += "<div class='c-ventana-modal__contenido'>" + contenido + "</div>"
     modal += "</div></div>"
     $(".l-distribucion").html(modal + $(".l-distribucion").html())
-    $('.js-ventana-modal__equis').on('click', cerrarVentanaModal);
+    $('.js-ventana-modal__cerrar').on('click', cerrarVentanaModal);
 }
 function cerrarVentanaModal() {
     $(".js-ventana-modal").remove()
