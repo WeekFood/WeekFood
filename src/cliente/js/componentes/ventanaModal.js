d9fd366771abb1e550324dce7f63af4ce419c21a
Hechix <jatonidonet@hotmail.com>
2019-01-12 23:23:06 +0100

    Cambios de diseño, prevencion de multiples ventantas, formateo

--- src/cliente/js/componentes/ventanaModal.js
+++ src/cliente/js/componentes/ventanaModal.js
@@ -85,6 +85,7 @@ function generarVentanaModal(opciones) {
     }
 
     modal += "</div></div>"
+    cerrarVentanaModal()
     $(".p-principal").prepend(modal)
     $('.js-ventana-modal__cerrar').on('click', () => { cerrarVentanaModal(); opciones.callback_Cerrar() });
     $('.js-ventana-modal__confirmar').on('click', () => { cerrarVentanaModal(); opciones.callback_Confirmar() });
