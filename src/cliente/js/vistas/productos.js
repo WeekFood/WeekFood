555880baff7ceb472e5413413b28d254dc40dc42
kokekui <kokekui17@gmail.com>
2019-01-12 23:51:10 +0100

    Arreglando grid

--- src/cliente/js/vistas/productos.js
+++ src/cliente/js/vistas/productos.js
@@ -1,22 +1,25 @@
 function vista_Productos(puntoMontaje) {
     montarMenu("/api/menu", "productos")
     var html = "";
+    html += "<div class='c-productos__container'>";
     return $.when($.getJSON("/api/productos").then((productos) => {
         productos.forEach(producto => {
-            html += "<div class='c-producto c-producto__container";
+            html += "<div class='c-principal c-producto__container";
             if (producto["destacado"] == 1) {
-            html += " c-producto--destacado'>";
-            html += "<img class='c-producto__imagen-destacado' src='/imagenes/estrella.png'>";
+            html += " c-producto--destacado'>";//Pensar en si se hace algo con esto
+            html += "<div class='c-producto__imagen-destacada'><i class='fas fa-star'></i></div>";
             html += "<img class='c-producto__imagen' src='/imagenes/productos/" + producto["foto"] + "'>";
-            html += "<img class='c-producto__carrito' src='/imagenes/carrito.png'>";
+            html += "<div class='c-producto__carrito'><i class='fas fa-shopping-cart'></i></div>";
             html += "<p class='c-producto__titulo'>" + producto["nombre"].charAt(0).toUpperCase() + producto["nombre"].slice(1) + "</p></div>";
             } else {
             html += "'>";
             html += "<img class='c-producto__imagen' src='/imagenes/productos/" + producto["foto"] + "'>";
-            html += "<img class='c-producto__carrito' src='/imagenes/carrito.png'>";
+            //html += "<img class='c-producto__carrito' src='/imagenes/carrito.png'>";
+            html += "<div class='c-producto__carrito'><i class='fas fa-shopping-cart'></i></div>";
             html += "<p class='c-producto__titulo'>" + producto["nombre"].charAt(0).toUpperCase() + producto["nombre"].slice(1) + "</p></div>";
             }
         })
+        html += "</div>";
     }
     )).then(() => {
         $(puntoMontaje).html(html);
