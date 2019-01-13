8e6c71399ff17018bf7442c0d4ea76f273bf6946
Hechix <jatonidonet@hotmail.com>
2019-01-13 21:49:08 +0100

    Carga segun la categoria y subcategoria

--- src/cliente/js/vistas/productos.js
+++ src/cliente/js/vistas/productos.js
@@ -4,14 +4,23 @@ function vista_Productos(puntoMontaje, categorias) {
     } else {
         vista_Productos_montarMenu(categorias);
     }
-    /*
+    $(puntoMontaje).html("")
     var url = "/api/productos"
     if (categorias) {
-        if ((categorias.hasOwnProperty("categoriaPrincipal")) && (categorias.hasOwnProperty("categoria"))) { url += "/categorias/" + categorias["categoriaPrincipal"] + "/" + categorias["categoria"] }
+        if (categorias.hasOwnProperty("subCategoria")) {
+            url += "/categorias/" + categorias["categoria"] + "/" + categorias["subCategoria"]
+            vista_Productos_cargarDe(puntoMontaje, url)
+        }else{
+            url += "/categorias/"
+            $.getJSON( url + categorias["categoria"]).then((cates)=>{
+                cates.forEach(cate => {
+                    vista_Productos_cargarDe(puntoMontaje, url +categorias["categoria"] +"/"+ cate["nombre"])
+                })
+            })
+        }
+    }else{
+        vista_Productos_cargarDe(puntoMontaje, url)
     }
-    $(puntoMontaje).html("")
-    vista_Productos_cargarDe(puntoMontaje, url)
-    */
 }
 function vista_Productos_cargarDe(puntoMontaje, url) {
     $.getJSON(url).then((productos) => {
@@ -28,61 +37,36 @@ function vista_Productos_cargarDe(puntoMontaje, url) {
     })
 }
 function vista_Productos_montarMenu(categorias) {
-    /*
-    $.getJSON("/api/productos/categorias/").then((cates) => {
-        var html = ""
-        cates.forEach(cate => {
-            html += "<li class='c-menu__item c-menu__sub' onclick='cargarVista(\"productos\",{\"categoriaPrincipal\" : \"" + cate["nombre"] + "\"})'>" + cate["nombre"] + "</li>"
-            if (categorias) {
-                if (categorias["categoriaPrincipal"] == cate["nombre"]) {
-                    html += "sDE30305Po7HtmTr" //esto sirve para guardar el sitio al menu interno
-                }
-            }
-        })
-        if (categorias) {
-            $.getJSON("/api/productos/categorias/" + categorias["categoriaPrincipal"]).then((cates) => {
-                var menuInterno = ""
-                cates.forEach(cate => {
-                    menuInterno += "<li class='c-menu__item c-menu__sub--2' onclick='cargarVista(\"productos\",{\"categoriaPrincipal\":\"" + categorias["categoriaPrincipal"] + "\",\"categoria\" : \"" + cate["nombre"] + "\"})'>" + cate["nombre"] + "</li>"
-                })
-                $(html.replace("sDE30305Po7HtmTr", menuInterno)).insertAfter(".js-menu-productos")
-            })
-        } else {
-
-            $(html).insertAfter(".js-menu-productos")
-        }
-    })*/
-    if (categorias.hasOwnProperty("categoriaPrincipal")) {
-        var nombreContenedor = "js-menu-productos__contenedor-secundario__" + categorias["categoriaPrincipal"]
+    if (categorias.hasOwnProperty("categoria")) {
+        var nombreContenedor = "js-menu-productos__contenedor-secundario__" + categorias["categoria"]
         if ($("." + nombreContenedor).length < 1) {
-            var nombreListado = "js-menu-productos__listado__" + categorias["categoriaPrincipal"]
+            var nombreListado = "js-menu-productos__listado__" + categorias["categoria"]
             var contenedorCategoriasSecundariaas = "<li class='" + nombreContenedor + "'><ul class='" + nombreListado + "'>"
             contenedorCategoriasSecundariaas += "</ul></li>"
-            $(contenedorCategoriasSecundariaas).insertAfter(".js-menu__productos--" + categorias["categoriaPrincipal"])
-            $.getJSON("/api/productos/categorias/" + categorias["categoriaPrincipal"]).then((cates) => {
+            $(contenedorCategoriasSecundariaas).insertAfter(".js-menu__productos--" + categorias["categoria"])
+            $.getJSON("/api/productos/categorias/" + categorias["categoria"]).then((cates) => {
                 cates.forEach(cate => {
-                    var categoriaNueva = `<li class='c-menu__item c-menu__sub--2 js-menu__productos__` + categorias["categoriaPrincipal"] + `--` + cate["nombre"] + `'
-                    onclick='cargarVista("productos",{"categoriaPrincipal" : "` + categorias["categoriaPrincipal"] + `","subCategoria":"` + cate["nombre"] + `"})'
+                    var categoriaNueva = `<li class='c-menu__item c-menu__sub--2 js-menu__productos__` + categorias["categoria"] + `--` + cate["nombre"] + `'
+                    onclick='cargarVista("productos",{"categoria" : "` + categorias["categoria"] + `","subCategoria":"` + cate["nombre"] + `"})'
                     >` + cate["nombre"] + `</li>`
                     $("." + nombreListado).append(categoriaNueva)
-                    console.log(nombreListado)
                 })
             })
-        }else{
-            $("."+nombreContenedor).remove()
+        } else {
+            $("." + nombreContenedor).remove()
         }
     } else {
         if ($(".js-menu-productos__contenedor").length < 1) {
             $.getJSON("/api/productos/categorias/").then((cates) => {
                 var contenedorCategoriasPrincipales = "<li class='js-menu-productos__contenedor'><ul>"
                 cates.forEach(cate => {
-                    var categoriaNueva = `<li class='c-menu__item c-menu__sub js-menu__productos--` + cate["nombre"] + `' onclick='cargarVista("productos",{"categoriaPrincipal" : "` + cate["nombre"] + `"})'>` + cate["nombre"] + `</li>`
+                    var categoriaNueva = `<li class='c-menu__item c-menu__sub js-menu__productos--` + cate["nombre"] + `' onclick='cargarVista("productos",{"categoria" : "` + cate["nombre"] + `"})'>` + cate["nombre"] + `</li>`
                     contenedorCategoriasPrincipales += categoriaNueva
                 })
                 contenedorCategoriasPrincipales += "</ul></li>"
                 $(contenedorCategoriasPrincipales).insertAfter(".js-menu-productos")
             })
-        }else{
+        } else {
             $(".js-menu-productos__contenedor").remove()
         }
     }
