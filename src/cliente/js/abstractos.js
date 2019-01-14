1a93fa42753f90a484553ca79f9a3c1a7506af3a
Hechix <jatonidonet@hotmail.com>
2019-01-14 20:19:21 +0100

    Eliminado debug

--- src/cliente/js/abstractos.js
+++ src/cliente/js/abstractos.js
@@ -4,30 +4,22 @@ class CacheJSONs {
     }
     getJSON(url) {
         var diferido = new $.Deferred();
-        console.log("buscando",url)
         var filtrados = this.jsonsEnCache.filter(this.filtrarJson.bind(this, url))
-        console.log("Existen",filtrados.length,"con url",url)
         if (filtrados.length < 1) {
-            console.log("se va a crear uno nuevo")
             diferido = this.nuevoJson(url, diferido)
         } else {
-            console.log("no se va a crear uno nuevo")
             if ((new Date().getTime() - filtrados[0].creacion) < 25000) {
-                console.log("hace menos de 25s")
                 diferido.resolve(filtrados[0].respuesta)
             } else {
-                console.log("hace mas de 25s")
                 diferido = this.nuevoJson(url, diferido)
             }
         }
         return diferido.promise();
     }
     filtrarJson(actual, buscando) {
-        console.log(actual, buscando)
         return actual == buscando["url"]
     }
     nuevoJson(url, diferido) {
-        console.log("Creando uno nuevo")
         var nuevoJsonCacheado = new JSONCacheado(url)
         nuevoJsonCacheado.respuesta = "holaQueTal"
         this.jsonsEnCache.push(nuevoJsonCacheado)
