92a8c4fd8cd03ce9b30ab0736463d2eaf24942d6
yuriyfpmislata <yuriy997+clase@gmail.com>
2019-01-14 11:02:16 +0100

    inicial Carrito, Producto, Articulo

---
+++ src/cliente/js/modelos/Producto.js
@@ -0,0 +1,14 @@
+class Producto {
+    /**
+     * @param {number} id 
+     * @param {string} nombre 
+     * @param {string} foto 
+     * @param {boolean} destacado 
+     */
+    constructor(id, nombre, foto, destacado) {
+        this.id = id;
+        this.nombre = nombre;
+        this.foto = foto;
+        this.destacado = destacado;
+    }
+}
