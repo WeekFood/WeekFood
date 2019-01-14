a91b4fe2774ddc8a05ed77552bcae084ae8a1f94
yuriyfpmislata <yuriy997+clase@gmail.com>
2019-01-14 23:08:08 +0100

    pruebas en init

--- src/cliente/js/init.js
+++ src/cliente/js/init.js
@@ -29,3 +29,22 @@ $('.js-boton-menu').on('click', () => {
 if (!redirigir()) {
     cargarVista("portada")
 }
+
+/* PRUEBAS CARRITO */
+
+let carrito = new Carrito();
+
+let productoDesdeAPI = new Producto(
+    1,
+    'Patatas fritas',
+    'patatas-fritas.png',
+    false
+);
+
+let articulo = new Articulo(productoDesdeAPI);
+
+carrito.añadirArticulo(articulo);
+
+console.dir(carrito.articulos);
+
+/* END PRUEBAS CARRITO */
