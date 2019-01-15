6cdec9ec4d9712b98d4e528b549b7fcc5f489366
Hechix <jatonidonet@hotmail.com>
2019-01-15 11:12:10 +0100

    Funcionalidad y estilo

--- src/cliente/js/componentes/carrito.js
+++ src/cliente/js/componentes/carrito.js
@@ -1,12 +1,19 @@
-function alternarCarrito(){
-    if ($(".c-carrito").length < 1){
+function alternarCarrito() {
+    if ($(".c-carrito").length < 1) {
         var html = "<div class='c-carrito c-principal'>"
         carrito.getArticulos().forEach(articulo => {
-            html += articulo.nombre
+            html += `<p class='c-carrito__articulo'>
+            <span class='c-carrito__nombre'>`+ articulo.nombre + `</span>
+            <span class='c-carrito__incrementar'><i class="fas fa-plus"></i></span>
+            <span class='c-carrito__cantidad'>`+ articulo.cantidad + `</span>
+            <span class='c-carrito__decrementar'><i class="fas fa-minus"></i></span>
+            <span class='c-carrito__basura'><i class="far fa-trash-alt"></i></span>
+            </p>`
         });
         html += "</div>"
         $(".p-principal").prepend(html)
-    }else{
-        $(".c-carrito").remove()
+    } else {
+        $(".c-carrito").addClass("c-carrito--desaparecer")
+        setTimeout(()=>{$(".c-carrito").remove()}, 501)
     }
 }
