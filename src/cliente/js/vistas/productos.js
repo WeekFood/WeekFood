cfd67b11517867a72663b2779d06a32aca67fd94
kokekui <kokekui17@gmail.com>
2019-01-15 22:29:44 +0100

    Falta anyadir el grid-gap

--- src/cliente/js/vistas/productos.js
+++ src/cliente/js/vistas/productos.js
@@ -1,7 +1,7 @@
 function vista_Productos(puntoMontaje) {
     montarMenu("/api/menu", "productos")
-    var containerDestacado = "<div class='c-productos__container c-productos__container--destacado'>";
-    var containerNoDestacado = "<div class='c-productos__container'>";
+    var containerDestacado = "<div class='c-productos'>";
+    var containerNoDestacado = "<div class='c-productos'>";
     return $.when( GLOBAL_CACHE_JSONS.getJSON("/api/productos").then((productos) => {
         productos.forEach(producto => {
             if (producto["destacado"] == 1) {
@@ -24,9 +24,8 @@ function vista_Producto_generarProducto(producto){
     html += "</i><img class='c-producto__imagen' src='/imagenes/productos/" + producto["foto"] + "'>";
     if (producto["destacado"] == 1) {
         html += "<div class='c-producto__imagen-destacado'><i class='fas fa-star fa-3x'></i></div>";
-        } 
-    //html += "<div class='c-producto__datos'>";
-    html += "<div class='c-producto__titulo-container'><p class='c-producto__titulo'>" + producto["nombre"].charAt(0).toUpperCase() + producto["nombre"].slice(1) + "</p></div>";
+        }
+    html += "<p class='c-producto__titulo'>" + producto["nombre"] + "</p>";
     html += "<div class='c-producto__precio'>2 €</div>";
     html += "<div class='c-producto__carrito'><i class='fas fa-cart-plus'></i></div></div>";
     return html;
