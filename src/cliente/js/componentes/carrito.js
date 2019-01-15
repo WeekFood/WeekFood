2b7e58f3a9572310c9ebf9900f0aebcce4cdec0e
Hechix <jatonidonet@hotmail.com>
2019-01-15 10:04:39 +0100

    wip

--- src/cliente/js/componentes/carrito.js
+++ src/cliente/js/componentes/carrito.js
@@ -1,8 +1,8 @@
 function alternarCarrito(){
     if ($(".c-carrito").length < 1){
         var html = "<div class='c-carrito c-principal'>"
-        carrito.getArticulos().forEach(element => {
-            html += ''
+        carrito.getArticulos().forEach(articulo => {
+            html += articulo.nombre
         });
         html += "</div>"
         $(".p-principal").prepend(html)
