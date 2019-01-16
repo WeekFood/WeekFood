6b8092c0677df0611a67e4ceee1ca33c2ba31f27
Hechix <jatonidonet@hotmail.com>
2019-01-16 20:59:22 +0100

    Puto seguimiento

---
+++ src/cliente/js/modelos/Categoria.js
@@ -0,0 +1,17 @@
+class Categoria{
+    /**
+     * 
+     * @param {String} nombre Nombre de la categoriaPrincipal
+     */
+    constructor(nombre){
+        this.nombre = nombre
+        this.categorias  = []
+    }
+    /**
+     * 
+     * @param {Array} categorias Array de categorias que se incluyen en esta categoria.} categorias 
+     */
+    añadirCategoria(categoria){
+        this.categorias.push(categoria)
+    }
+} 
