2607eeeca05d552de97977e3251f2cdc3864cc84
jlsanchezdelgado95 <kokekui17@gmail.com>
2019-01-16 09:46:49 +0100

    feat(#68): Grid Producto (#121)
    * Preparado para retocar grid
    
    * arreglar altura
    
    * Grid productos finalizado
    
    * Arreglando grid
    
    * calculando grid
    
    * Colocar estrella y datos de producto correctamente
    
    * Anyadiendo padding y tamanyo a destacado
    
    * Falta cuadrar los datos
    
    * Margin añadido
    
    * Falta el segundo grid
    
    * Preparando grid datos producto
    
    * actualizada
    
    * actualizada + destacados
    
    * Problema con el container producto, corregir
    
    * Grid productos finalizado
    
    * Grid Productos para implementer
    
    * Falta anyadir el grid-gap
    
    * gap anyadido
    
    * Eliminado media only

--- src/cliente/js/vistas/productos.js
+++ src/cliente/js/vistas/productos.js
@@ -1,18 +1,32 @@
 function vista_Productos(puntoMontaje) {
     montarMenu("/api/menu", "productos")
-    var html = "";
+    var containerDestacado = "<div class='c-productos'>";
+    var containerNoDestacado = "<div class='c-productos'>";
     return $.when( GLOBAL_CACHE_JSONS.getJSON("/api/productos").then((productos) => {
         productos.forEach(producto => {
-            html += "<div class='c-principal c-producto";
             if (producto["destacado"] == 1) {
-                html += " c-producto--destacado'> <img class='c-producto__imagen-destacado' src='imagenes/estrella.png";
+                containerDestacado += vista_Producto_generarProducto(producto);
+            } else {
+                containerNoDestacado += vista_Producto_generarProducto(producto);
             }
-            html += "'><img class='c-producto__imagen' src='/imagenes/productos/" + producto["foto"] + "'>";
-            html += "<p class='c-producto__titulo'>" + producto["nombre"].charAt(0).toUpperCase() + producto["nombre"].slice(1) + "</p></div>";
         })
+        containerDestacado += "</div>";
+        containerNoDestacado += "</div>";
     }
     )).then(() => {
-        $(puntoMontaje).html(html);
+        $(puntoMontaje).html(containerDestacado + containerNoDestacado);
     })
+}
 
+function vista_Producto_generarProducto(producto){
+    var html = "";
+    html += "<div class='c-producto'>";
+    html += "</i><img class='c-producto__imagen' src='/imagenes/productos/" + producto["foto"] + "'>";
+    if (producto["destacado"] == 1) {
+        html += "<div class='c-producto__imagen-destacado'><i class='fas fa-star fa-3x'></i></div>";
+        }
+    html += "<p class='c-producto__titulo'>" + producto["nombre"] + "</p>";
+    html += "<div class='c-producto__precio'>2 €</div>";
+    html += "<div class='c-producto__carrito'><i class='fas fa-cart-plus'></i></div></div>";
+    return html;
 }
