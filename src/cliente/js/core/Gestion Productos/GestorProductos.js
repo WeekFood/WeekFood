78310c4f930424c4a15bb6a8ed01034929057f7f
Hechix <jatonidonet@hotmail.com>
2019-01-16 09:59:08 +0100

    ventana modal

--- src/cliente/js/core/Gestion Productos/GestorProductos.js
+++ src/cliente/js/core/Gestion Productos/GestorProductos.js
@@ -67,18 +67,13 @@ class GestorProductos {
             return $.when(productosFiltrados)
         } else {
             return GLOBAL_CACHE_JSONS.getJSON("/api/productos/categorias/" + categoriaPrincipal + "/" + categoria).then((respuesta) => {
-                if (respuesta !== null) {
-                    var nuevosProductos = []
-                    respuesta.forEach(prod => {
-                        var nuevoProducto = new Producto(prod.id, prod.categoria.split(","), prod.nombre, prod.descripcion, prod.foto, (prod.destacado == 1))
-                        this.productos.push(nuevoProducto)
-                        nuevosProductos.push(nuevoProducto)
-                    });
-                    return nuevosProductos
-                } else {
-                    console.error(categoriaPrincipal + "/" + categoria, "es nulo.")
-                    return null
-                }
+                var nuevosProductos = []
+                respuesta.forEach(prod => {
+                    var nuevoProducto = new Producto(prod.id, prod.categoria.split(","), prod.nombre, prod.descripcion, prod.foto, (prod.destacado == 1))
+                    this.productos.push(nuevoProducto)
+                    nuevosProductos.push(nuevoProducto)
+                });
+                return nuevosProductos
             })
         }
     }
@@ -95,5 +90,36 @@ class GestorProductos {
             throw "El gestor esta inicializandose";
         }
         return this.categoriasPrincipales.map(cate => cate.nombre)
+    }
+    /**
+     * 
+     * @param {Int} id Id del producto a generar la ventana modal.
+     */
+    generarVentanaModalId(id, tipo = "info", callback_Confirmar = ()=>{}, callback_Denegar= ()=>{}){
+        var producto = this.getProductoId(id)
+        if (producto == undefined) {throw "El producto no existe."}
+        var html = `
+        <div>
+        <div>
+        <img src='/imagenes/productos/`+producto.foto+`'>
+        </div>
+        <div>
+        <p>`+producto.descripcion+`</p>
+        <div>`
+        producto.categorias.forEach(categoria=>{
+            html +=`<span>`+categoria+`</span>`
+        })
+        html += `</div>
+        </div>
+        </div>
+        `
+        generarVentanaModal({
+            tamaño : "grande",
+            tipo : "confirmacion",
+            titulo : producto.nombre,
+            contenido : html,
+            callback_Confirmar : callback_Confirmar,
+            callback_Denegar : callback_Denegar
+        })
     }
 } 
