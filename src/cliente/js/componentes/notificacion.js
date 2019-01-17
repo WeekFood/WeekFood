736f7849d243fdbb81c7583ed01320c3478b85a0
Hechix <jatonidonet@hotmail.com>
2019-01-17 19:24:39 +0100

    Añadida variable global para evitar el cierre abbrupto de notificaciones

--- src/cliente/js/componentes/notificacion.js
+++ src/cliente/js/componentes/notificacion.js
@@ -3,16 +3,18 @@ function generarNotificacion(contenido, flash = false) {
     if (flash) {
         notificacion += "c-notificacion c-notificacion--flash js-notificacion--flash'>"
         $(".js-notificacion--flash").remove()
-        setTimeout(() => { $(".js-notificacion--flash").remove() }, 5010)
+        clearTimeout(GLOBAL_NOTIFICACION_TOP)
+        GLOBAL_NOTIFICACION_TOP = setTimeout(() => { $(".js-notificacion--flash").remove() }, 5010)
     }
     else {
         notificacion += "c-notificacion c-notificacion--top js-notificacion--top'>"
         $(".js-notificacion--top").remove()
-        setTimeout(() => { $(".js-notificacion--top").remove() }, 10010)
+        clearTimeout(GLOBAL_NOTIFICACION_FLASH)
+        GLOBAL_NOTIFICACION_FLASH = setTimeout(() => { $(".js-notificacion--top").remove() }, 10010)
     }
     notificacion += contenido + "</div>"
     $(".p-principal").prepend(notificacion)
-    $(".js-notificacion--top").on('click',()=>{$(".js-notificacion--top").remove()})
-    $(".js-notificacion--flash").on('click',()=>{$(".js-notificacion--flash").remove()})
+    $(".js-notificacion--top").on('click', () => { $(".js-notificacion--top").remove() })
+    $(".js-notificacion--flash").on('click', () => { $(".js-notificacion--flash").remove() })
 
 }
