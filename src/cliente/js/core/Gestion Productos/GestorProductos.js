0fe765ac15f5bcae55bde0da9f0eaca8cda7205a
Hechix <jatonidonet@hotmail.com>
2019-01-17 12:43:54 +0100

    cambio int a sting por consistencia

--- src/cliente/js/core/Gestion Productos/GestorProductos.js
+++ src/cliente/js/core/Gestion Productos/GestorProductos.js
@@ -77,7 +77,7 @@ class GestorProductos {
     }
     /**
      * 
-     * @param {int} id Id a buscar
+     * @param {string} id Id a buscar
      */
     getProductoId(id) {
         return this.productos.find(producto => this.filtrarId(id, producto))
@@ -88,7 +88,7 @@ class GestorProductos {
     }
     /**
     * 
-    * @param {Int} id Id del producto a generar la ventana modal.
+    * @param {string} id Id del producto a generar la ventana modal.
     * @param {String} tipo tipo de la ventana a generar, defecto info
     * @param {function} callback_Confirmar callback del boton confirmar en caso de que exista, defecto funcion vacia
     * @param {function} callback_Denegar callback del boton denegar en caso de que exista, defecto funcion vacia
