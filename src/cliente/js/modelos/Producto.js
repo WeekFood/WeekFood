5eafbd25a3ec1a4de3a536caeb5d6bcd4c310249
Hechix <jatonidonet@hotmail.com>
2019-01-17 07:42:13 +0100

    Actualizado producto

--- src/cliente/js/modelos/Producto.js
+++ src/cliente/js/modelos/Producto.js
@@ -2,18 +2,20 @@ class Producto{
     /**
      * 
      * @param {int} id Id del producto, proveniente de la API.
-     * @param {array} categorias Categorias del producto, proveniente de la API.
      * @param {string} nombre Nombre del producto, proveniente de la API.
-     * @param {string} descripcion Dewcripcion del producto, proveniente de la API.
      * @param {string} foto Foto del producto, proveniente de la API.
      * @param {String} destacado Categoria principal a buscar, requiere llamada a la API.
+     * @param {array} categorias Categorias del producto, proveniente de la API.
+     * @param {string} descripcion Descripcion del producto, proveniente de la API
+     * @param {int} precio Precio del producto, proveniente de la API
      */
-    constructor(id,categorias,nombre,descripcion,foto,destacado){
+    constructor(id,nombre,foto,destacado,categorias,descripcion,precio){
         this.id = id
-        this.categorias = categorias
         this.nombre = nombre
-        this.descripcion = descripcion
         this.foto = foto
         this.destacado = destacado
+        this.categorias = categorias
+        this.descripcion = descripcion
+        this.precio = precio
     }
 } 
