570c85297a046c43353e5beb8c3e1989d68dbc90
Hechix <jatonidonet@hotmail.com>
2019-01-20 15:35:49 +0100

    fix(#176) : Solucionado, se reescribia el html y por eso no se veian las clases

--- src/cliente/js/componentes/carrito.js
+++ src/cliente/js/componentes/carrito.js
@@ -4,7 +4,7 @@ function carrito_Alternar() {
     } else {
         $(".c-carrito").toggleClass("c-carrito--desaparecer")
         $(".js-carrito").children(".c-carrito__notificacion").remove()
-    } 
+    }
     $(".js-carrito").children("i").toggleClass("fa-angle-up").toggleClass("fa-shopping-cart")
     carrito_Actualizar()
 }
@@ -17,7 +17,7 @@ function carrito_Actualizar() {
         html += `
             <p class='c-carrito__articulo c-carrito__botones'>
             <span class='c-boton c-boton--basico js-carrito-ver-carrito'>(`+ carrito.getArticulos().length + `) Ver mi carrito </span>
-            <span class='c-boton c-boton--exito js-carrito-pagar'>`+precioEnEuros(carrito.getImporteTotal()) + `</span>
+            <span class='c-boton c-boton--exito js-carrito-pagar'>`+ precioEnEuros(carrito.getImporteTotal()) + `</span>
             </p>
             `
     }
@@ -29,62 +29,60 @@ function carrito_AñadirArticulo(evento) {
     var producto = GLOBAL_GESTOR_PRODUCTOS.getProductoId($(this).parent().data('id'))
     carrito.añadirProducto(producto)
     carrito_Actualizar()
-    producto =carrito.getArticulo(producto.id)
-    if (producto.cantidad > 1){
-        generarNotificacion(producto.nombre+" tienes "+producto.cantidad +" unidades.",true)
-    }else{
-        generarNotificacion(producto.nombre+" añadido al carrito.",true)
+    producto = carrito.getArticulo(producto.id)
+    if (producto.cantidad > 1) {
+        generarNotificacion(producto.nombre + " tienes " + producto.cantidad + " unidades.", true)
+    } else {
+        generarNotificacion(producto.nombre + " añadido al carrito.", true)
     }
-    if ((carrito.getArticulos().length == 1 && $(".c-carrito").hasClass('c-carrito--desaparecer'))||($(".c-carrito").length < 1)){
-        carrito_Alternar() 
+    if ((carrito.getArticulos().length == 1 && $(".c-carrito").hasClass('c-carrito--desaparecer')) || ($(".c-carrito").length < 1)) {
+        carrito_Alternar()
         $(".js-carrito").prepend(`<div class='c-carrito__notificacion'><i class="fas fa-bell"></i></div>`)
     }
-    else if($(".c-carrito").hasClass('c-carrito--desaparecer')){
+    else if ($(".c-carrito").hasClass('c-carrito--desaparecer')) {
         $(".js-carrito").prepend(`<div class='c-carrito__notificacion'><i class="fas fa-bell"></i></div>`)
     }
 }
 function carrito_ProcesarArticulo(articulo) {
     var html = `<p data-id='` + articulo.id + `' class='c-carrito__articulo'>
     <span class='c-carrito__nombre js-carrito-nombre'>`+ articulo.nombre + `</span>
-    <span class='c-carrito__operador js-carrito-incremento'><i class="fas fa-plus"></i></span>
-    <span class='c-carrito__cantidad js-carrito-cantidad'>`+ articulo.cantidad + `</span>
-    <span class='c-carrito__operador js-carrito-decremento'><i class="fas fa-minus"></i></span>
+    <span class='c-carrito__operador js-carrito-incremento`
+    if (articulo.cantidad >= Carrito.CANTIDAD_MAXIMA) {
+        html += ' c-carrito__operador--limite'
+    }
+    html += `'><i class="fas fa-plus"></i></span>
+    <span class='c-carrito__cantidad js-carrito-cantidad`
+    if (articulo.cantidad <= Carrito.CANTIDAD_MINIMA || articulo.cantidad >= Carrito.CANTIDAD_MAXIMA) {
+        html += ' c-carrito__cantidad--limite'
+    }
+    html += `'>` + articulo.cantidad + `</span>
+    <span class='c-carrito__operador js-carrito-decremento`
+    if (articulo.cantidad <= Carrito.CANTIDAD_MINIMA) {
+        html += ' c-carrito__operador--limite'
+    }
+    html += `'><i class="fas fa-minus"></i></span>
     <span class='c-carrito__basura js-carrito-basura'><i class="far fa-trash-alt"></i></span>
     </p>`
     return html
 }
 function carrito_QuitarArticulo(evento) {
     var producto = vista_Productos_existeEnGrid($(this).parent().data('id'))
-    if (producto){
+    if (producto) {
         $(producto).children('.js-producto-carrito').removeClass('c-producto__carrito--en-carrito')
     }
     producto = GLOBAL_GESTOR_PRODUCTOS.getProductoId($(this).parent().data('id'))
     carrito.quitarArticulo(producto.id)
     carrito_Actualizar()
-    generarNotificacion(producto.nombre+" eliminado del carrito.",true)
+    generarNotificacion(producto.nombre + " eliminado del carrito.", true)
 }
 function carrito_IncrementarArticulo(evento) {
     var cantidadActual = carrito.incrementarCantidad($(this).parent().data('id'))
     $(this).parent().find('.js-carrito-cantidad').html(cantidadActual)
-    if (cantidadActual >= Carrito.CANTIDAD_MAXIMA){
-        $(this).addClass('c-carrito__operador--limite')
-        $($(this).parent().find('.js-carrito-cantidad')).addClass("c-carrito__cantidad--limite")
-    }else{
-        $(this).parent().find('.js-carrito-decremento').removeClass('c-carrito__operador--limite')
-        $(this).parent().find('.js-carrito-cantidad').removeClass("c-carrito__cantidad--limite")
-    }
     carrito_Actualizar()
 }
 function carrito_DecrementarArticulo(evento) {
     var cantidadActual = carrito.decrementarCantidad($(this).parent().data('id'))
     $(this).parent().find('.js-carrito-cantidad').html(cantidadActual)
-    if (cantidadActual <= Carrito.CANTIDAD_MINIMA){
-        $(this).addClass('c-carrito__operador--limite')
-        $(this).parent().find('.js-carrito-cantidad').addClass("c-carrito__cantidad--limite")
-    }else{
-        $(this).parent().find('.js-carrito-incremento').removeClass('c-carrito__operador--limite')
-        $(this).parent().find('.js-carrito-cantidad').removeClass("c-carrito__cantidad--limite")
-    }
     carrito_Actualizar()
 }
 function carrito_ActualizarTriggers() {
