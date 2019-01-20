e967fa0343a7e969f17321ad55bf23e3c7163504
yuriyfpmislata <yuriy997+clase@gmail.com>
2019-01-20 14:22:59 +0100

    wip index: deindentado

--- src/index.php
+++ src/index.php
@@ -1,35 +1,35 @@
 <?php
-    error_reporting(E_ALL);
-    ini_set('display_errors', 1);
-    $config = require_once "./configs/config.php";
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+$config = require_once "./configs/config.php";
 
-    try {
-        $mysql = new PDO(
-            "mysql:dbname=" . $config['db']['name'] . ";host=" . $config['db']['host'] . ";port=".$config['db']['port'],
-            $config['db']['user'],
-            $config['db']['password']
-        );
-        $mysql->exec("set names utf8");
-        $mysql->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    } catch (PDOException $e) {
-        echo "<p>Error: Cannot connect to database server.</p>\n";
-        echo $e;
-        exit();
-    }
+try {
+    $mysql = new PDO(
+        "mysql:dbname=" . $config['db']['name'] . ";host=" . $config['db']['host'] . ";port=".$config['db']['port'],
+        $config['db']['user'],
+        $config['db']['password']
+    );
+    $mysql->exec("set names utf8");
+    $mysql->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo "<p>Error: Cannot connect to database server.</p>\n";
+    echo $e;
+    exit();
+}
 
-    require_once("./core/AutoLoad.php");
+require_once("./core/AutoLoad.php");
 
-    use \core\MVC\Controller;
+use \core\MVC\Controller;
 
-    $globals = \core\Globals::getInstance();
+$globals = \core\Globals::getInstance();
 
-    $globals->set("db", $mysql);
+$globals->set("db", $mysql);
 
-    //Utilizar el método set de $globals para añadir $config ($key será "config")
-    $globals->set("config", $config);
+//Utilizar el método set de $globals para añadir $config ($key será "config")
+$globals->set("config", $config);
 
-    //Crear un nuevo objeto de tipo Controller
-    $controller = new Controller();
+//Crear un nuevo objeto de tipo Controller
+$controller = new Controller();
 
-    //Llamar al método run del objeto creado anteriormente
-    $controller->run();
+//Llamar al método run del objeto creado anteriormente
+$controller->run();
