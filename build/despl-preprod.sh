b7ef3ff781f7bd9bd4f053dffa968815e7b6e341
yuriyfpmislata <yuriy997+clase@gmail.com>
2019-01-24 09:38:58 +0100

    para testear

--- build/despl-preprod.sh
+++ build/despl-preprod.sh
@@ -4,5 +4,6 @@ echo "Copiando dist/ a ../../www"
 rm ../../www/* -rf && 
 cp ./build/config/htaccess ../../www/.htaccess &&
 cp ./dist/* ../../www -r &&
+rm ../../www/configs/prod_config.php &&
 mv ../../www/configs/preprod_config.php ../../www/configs/config.php
 echo "Copiado correctamente."
