5f53b1971bab4501cb88a9205fa0c7678d7223ea
yuriy <36030998+yuriyfpmislata@users.noreply.github.com>
2019-01-24 12:19:27 +0100

    feat: Script despliegue Producción (#216)
    * para testear
    
    * faltaban scripts npm
    
    * prod_config: actualizado nombre contenedor al mismo que preprod

---
+++ src/configs/prod_config.php
@@ -0,0 +1,19 @@
+<?php
+
+define('DS', DIRECTORY_SEPARATOR);
+define('ROOT', $_SERVER['DOCUMENT_ROOT']);
+
+return [
+    'db' => [
+        'name' => 'WeekFood',
+        'host' => 'weekfood_db',
+        'port' => '3306',
+        'user' => 'WeekFood',
+        'password' => 'SQITBAxsMBPuQtaZ'
+    ],
+    'site' => [
+        'coreMVC' => ROOT . DS . 'core' . DS . 'MVC' . DS,
+        'configs' => ROOT . DS . 'configs' . DS,
+        'resources' => ROOT . DS . 'app' . DS . 'resources' . DS
+    ]
+];
