69beb62e9ebf8d29cb63281c5c71b3355f116482
Hechix <jatonidonet@hotmail.com>
2019-01-25 20:10:13 +0100

    Wip animaciones

--- src/cliente/js/componentes/acceso.js
+++ src/cliente/js/componentes/acceso.js
@@ -1,12 +1,14 @@
 function acceso_Alternar() {
-    if ($(".c-acceso").length < 1) {
-        $(".p-principal").prepend(`<div class='c-acceso'></div>`)
+    if ($(".js-registro").length < 1) {
+        console.log("Añadiendo boton registro")
         $(".c-cabecera__botones").prepend('<div class="c-cabecera__boton js-registro"><i class="fas fa-user-plus"></i></div>')
         $(".js-registro").on('click', acceso_AlternarRegistro)
-    } else {
-        $(".c-acceso").toggleClass("c-acceso--desaparecer")
     }
-    $(".js-acceso").children("i").toggleClass("fa-angle-up").toggleClass("fa-user")
+    if (!acceso_AlternarLogin()) {
+        console.log("Si se ha subido login, borrar registro")
+        $(".js-acceso__panel").remove()
+        $(".js-registro").remove()
+    }
 }
 function acceso_AlternarRegistro() {
     if ($(".c-carrito").length == 1) {
@@ -14,6 +16,17 @@ function acceso_AlternarRegistro() {
             carrito_Alternar()
         }
     }
+    $(".js-acceso__panel").remove()
+    $(".js-acceso").children("i").toggleClass("fa-angle-up").toggleClass("fa-user")
+    $(".p-principal").prepend(`<div class='c-acceso__panel js-acceso__panel'>
+        <p class="c-acceso__titulo">Nuevo usuario</p>
+        <input class="c-acceso__campo" id="acceso-login__nombre" type="text" placeholder="Usuario">
+        <br><input class="c-acceso__campo" id="acceso-login__contra" type="text" placeholder="Contraseña">
+        <br><input class="c-acceso__campo" id="acceso-login__contra-repe" type="text" placeholder="Contraseña">
+        <br><div class="c-boton c-boton--basico c-acceso__boton">Registro</div>
+        </div>`)
+    $(".js-registro").children("i").toggleClass("fa-angle-up").toggleClass("fa-user-plus")
+    return true
 }
 function acceso_AlternarLogin() {
     if ($(".c-carrito").length == 1) {
@@ -21,4 +34,16 @@ function acceso_AlternarLogin() {
             carrito_Alternar()
         }
     }
+    if($(".js-registro").children("i").hasClass("fa-angle-up")){
+        $(".js-registro").children("i").toggleClass("fa-angle-up").toggleClass("fa-user-plus")
+    }
+    $(".js-acceso__panel").remove()
+    $(".p-principal").prepend(`<div class='c-acceso__panel js-acceso__panel'>
+        <p class="c-acceso__titulo">Acceso</p>
+        <input class="c-acceso__campo" id="acceso-login__nombre" type="text" placeholder="Usuario">
+        <br><input class="c-acceso__campo" id="acceso-login__contra" type="text" placeholder="Contraseña">
+        <br><div class="c-boton c-boton--basico c-acceso__boton">Entrar</div>
+        </div>`)
+    $(".js-acceso").children("i").toggleClass("fa-angle-up").toggleClass("fa-user")
+    return $(".js-acceso").children("i").hasClass("fa-angle-up")
 }
