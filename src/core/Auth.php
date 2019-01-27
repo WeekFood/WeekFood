1014bf987e6c81582883654ce831275dad3128b1
yuriyfpmislata <yuriy997+clase@gmail.com>
2019-01-27 12:10:02 +0100

    PoC inicial: register, login, setCookies, generateRandomToken
    a falta de testeo

--- src/core/Auth.php
+++ src/core/Auth.php
@@ -1,14 +1,19 @@
 <?php
 namespace core;
 
-use core\Globals as Globals;
-
 class Auth {
     const ERR_NO_TOKEN = 'ERR_NO_TOKEN';
     const ERR_ROLE_FORBIDDEN = 'ERR_ROLE_FORBIDDEN';
     const ERR_ACCESS_FORBIDDEN = 'ERR_ACCESS_FORBIDDEN';
+    const ERR_LOGIN_USER_NOT_FOUND = 'ERR_LOGIN_USER_NOT_FOUND';
 
     const RESOURCE_CARRITO_ID = 'RESOURCE_CARRITO_ID';
+
+    private const PRIVATE_KEY = "F4Ev-17IbLRcEkwr2p8NRL62bys5fo6AqJrfWZwd5wBUBqDdDueKZz4VlJiWaD1TOXkmNtrU2gCmhNeZvimikm-3yI293zaufdnSoJ0isJ_i1SDmR8GeWVTVkBIPRewP4yBlb2uHbm1Uxppd0wkFau8iNmm5tqQppG0O5Rij5oojForsrvT8ahB9YYkX3fbM5u0RAW4AHbXqrN62xlN17FuXzZUtknI_W_HSOnnrQH5Rj0ZaT2GzRdR9PyaoXfLEduCq_2NowAxIzznsn-OnTFf7VuSrqmj5z1cvO_qyGM0sDNJiUQjKV-R-FQYK9yBkWsWclncU7CVN8uz44CSQng";
+
+    private const COOKIE_LIFETIME_SEC = 60 * 60 * 24 * 7; /* 7 days */
+    private const COOKIE_NAME_TOKEN = 'token';
+    private const COOKIE_NAME_REMEMBER_ME = 'recuerdame';
 
     private $db = null;
 
@@ -16,8 +21,87 @@ class Auth {
         $this->db = $db;
     }
 
-    public function login(string $user, string $password, bool $rememberMe) {
-        // TODO
+    public function register(string $nick, string $password, string $name, bool $rememberMe = false) {
+        $hashedPassword = password_hash($password, PASSWORD_BCRYPT);
+
+        $sql = 'INSERT INTO
+                    usuarios (nick, contraseña, nombre)
+                VALUES (
+                    :nick,
+                    :contraseña,
+                    :nombre
+                );';
+
+        $ps = $this->db->prepare($sql);
+        $ps->bindParam(':nick', $nick);
+        $ps->bindParam(':contraseña', $password);
+        $ps->bindParam(':nombre', $name);
+
+        $ps->execute();
+
+        // TODO: if insertado con exito ?
+        // TODO: throw error si ya existe
+
+        $this->login($nick, $password, $rememberMe);
+    }
+
+    public function login(string $nick, string $password, bool $rememberMe = false): bool {
+        $sql = 'SELECT * FROM usuarios WHERE nick = :nick LIMIT 1;';
+
+        $ps = $this->db->prepare($sql);
+        $ps->bindParam(':nick', $nick);
+        $ps->execute();
+
+        $result = $ps->fetch();
+
+        if (!empty($result)) {
+            $dbHashedPassword = $result['contraseña'];
+            
+            if (password_verify($password, $dbHashedPassword)) {
+                return $this->setCookies($result['id'], true);
+            }
+        } else {
+            $this->sendError(self::ERR_LOGIN_USER_NOT_FOUND);
+            return false;
+        }
+    }
+
+    private function setCookies(string $userId, bool $rememberMe): bool {
+        session_start();
+
+        // TODO: asignar a SESSION el id de usuario y ¿ nivel de privilegio ?
+
+        if ($rememberMe) {
+            $token = $this->generateRandomToken();
+
+            $body = $userId . ':' . $token;
+            $bodySignedHash = hash_hmac('sha256', $body, self::PRIVATE_KEY);
+
+            $cookie = $boby . ':' . $bodySignedHash;
+
+            /* TODO: movida dominio localhost [1] */
+            // https://stackoverflow.com/a/1188145/3499595
+            $domain = ($_SERVER['HTTP_HOST'] != 'localhost') ? $_SERVER['HTTP_HOST'] : false;
+
+            setcookie(
+                self::COOKIE_NAME_TOKEN,
+                $cookie,
+                time() + self::COOKIE_LIFETIME_SEC
+                /* TODO: movida dominio localhost [2] necesario para asignar httponly */
+            );
+
+            setcookie(
+                self::COOKIE_NAME_REMEMBER_ME,
+                '',
+                time() + self::COOKIE_LIFETIME_SEC
+            );
+        }
+
+        return true;
+    }
+
+    private function generateRandomToken(): string {
+        return bin2hex(random_bytes(32));
     }
 
     public function isAuthenticated(): bool {
@@ -37,15 +121,16 @@ class Auth {
 
     public function sendError(string $errorConst) {
         switch ($type) {
-            case (Auth::ERR_NO_TOKEN):
-                http_response_code(401);
-                break;
-            case (Auth::ERR_ROLE_FORBIDDEN):
-            case (Auth::ERR_ACCESS_FORBIDDEN):
-                http_response_code(403);
-                break;
-            default:
-                http_response_code(500);
+        case (self::ERR_NO_TOKEN):
+        case (self::ERR_LOGIN_USER_NOT_FOUND):
+            http_response_code(401);
+            break;
+        case (self::ERR_ROLE_FORBIDDEN):
+        case (self::ERR_ACCESS_FORBIDDEN):
+            http_response_code(403);
+            break;
+        default:
+            http_response_code(500);
         }
     }
 }
