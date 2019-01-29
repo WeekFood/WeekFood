910f3fc6636fc81e4a0a118d546e981239896896
yuriyfpmislata <yuriy997+clase@gmail.com>
2019-01-29 09:54:16 +0100

    validacion de registro y login

--- src/app/resources/AuthResource.php
+++ src/app/resources/AuthResource.php
@@ -1,11 +1,11 @@
 <?php
-
-use core\MVC\Resource as Resource;
-use core\Globals;
 use core\Auth\Auth;
+use core\Auth\NickTakenException;
 use core\Auth\UserNotFoundException;
 use core\Auth\WrongPasswordException;
-use core\Auth\NickTakenException;
+
+use core\Globals;
+use core\MVC\Resource;
 
 class AuthResource extends Resource {
     /** @var Auth  */
@@ -16,8 +16,28 @@ class AuthResource extends Resource {
     }
 
     public function postRegistroAction() {
+
+        $LONGITUD_MINIMA_NICK = 4;
+        $LONGITUD_MINIMA_CONTRASEÑA = 6;
+
         if (!isset($_POST['nick']) || !isset($_POST['contraseña'])) {
+            // TODO: validar nombre cuando el frontend lo tenga en cuenta
             $this->setError(400, 'FALTAN_CAMPOS');
+            return;
+        }
+
+        if (empty($_POST['nick']) || empty($_POST['contraseña'])) {
+            // TODO: validar nombre cuando el frontend lo tenga en cuenta
+            $this->setError(400, 'CAMPOS_VACIOS');
+            return;
+        }
+
+        if (
+            strlen($_POST['nick']) < $LONGITUD_MINIMA_NICK
+                || 
+            strlen($_POST['contraseña']) < $LONGITUD_MINIMA_CONTRASEÑA
+        ) {
+            $this->setError(400, 'CAMPOS_INVALIDOS');
             return;
         }
 
@@ -36,13 +56,18 @@ class AuthResource extends Resource {
             header('HTTP/1.1 201 Created', true, 201);
             $this->setData();
         } catch (NickTakenException $e) {
-            $this->setError(409, 'NICK_YA_EXISTE');
+            $this->setError(409, 'USUARIO_YA_EXISTE');
         }
     }
 
     public function postLoginAction() {
         if (!isset($_POST['nick']) || !isset($_POST['contraseña'])) {
             $this->setError(400, 'FALTAN_CAMPOS');
+            return;
+        }
+
+        if (empty($_POST['nick']) || empty($_POST['contraseña'])) {
+            $this->setError(400, 'CAMPOS_VACIOS');
             return;
         }
 
