afc55a6d6b08d3384565597051db3a56c2a3dd35
jlsanchezdelgado95 <kokekui17@gmail.com>
2019-01-31 11:50:05 +0100

    Correcciones de inutil realizadas

--- src/cliente/js/vistas/multilenguaje.js
+++ src/cliente/js/vistas/multilenguaje.js
@@ -7,7 +7,7 @@ function vista_QuienesSomos(puntoMontaje, idioma) {
             <p class='c-quienes-somos__titulo'>Quiénes somos</p>
             </div>
             <div class='c-quienes-somos__contenido-interno'>
-            <p class='c-quienes-somos__texto'>Somos una empresa que gestiona el reparto de casas de comida.</p>
+            <p class='c-quienes-somos__texto'>Somos una empresa ficticia que gestiona el reparto de casas de comida.</p>
             <p class='c-quienes-somos__texto'>Colaboramos con casas de comida locales para ofrecer el mejor servicio.</p>
             <p class='c-quienes-somos__texto'>Come casero, come barato, come a tu hora, come WeekFood.</p>
             </div>
@@ -18,13 +18,13 @@ function vista_QuienesSomos(puntoMontaje, idioma) {
             </div>
             <div class='c-quienes-somos__servicios'>
             <div class='c-quienes-somos__attCliente'>
-            <p class='c-quienes-somos__titulo-menor'>Contacta con nosotros.</p>
+            <p class='c-quienes-somos__titulo-menor'>Contacta con nosotros</p>
             <div class='c-quienes-somos__servicios-container'><div class='c-quienes-somos__servicios-icono'><i class="fas fa-phone"></i></div><div class='c-quienes-somos__texto-icono'>902 - 123- 469</div></div>
             <div class='c-quienes-somos__servicios-container'><div class='c-quienes-somos__servicios-icono'><i class="far fa-envelope"></i></div><div class='c-quienes-somos__texto-icono'>info@weekfood.com</div></div>
             <div class='c-quienes-somos__servicios-container'><div class='c-quienes-somos__servicios-icono'><i class="fab fa-whatsapp"></i></div><div class='c-quienes-somos__texto-icono'>603 - 123- 469</div></div>
             </div>
             <div class='c-quienes-somos__redes'>
-            <p class='c-quienes-somos__titulo-menor'>¡Siguenos en nuestras redes sociales!</p>
+            <p class='c-quienes-somos__titulo-menor'>¡Síguenos en nuestras redes sociales!</p>
             <div class='c-quienes-somos__servicios-container'><div class='c-quienes-somos__servicios-icono'><i class="fab fa-instagram"></i></div><div class='c-quienes-somos__texto-icono'>@WeekFood</div></div>
             <div class='c-quienes-somos__servicios-container'><div class='c-quienes-somos__servicios-icono'><i class="fab fa-twitter"></i></div></i><div class='c-quienes-somos__texto-icono'>@WeekFood</div></div>
             <div class='c-quienes-somos__servicios-container'><div class='c-quienes-somos__servicios-icono'><i class="fab fa-facebook"></i></div><div class='c-quienes-somos__texto-icono'>/WeekFood</div></div>
@@ -34,24 +34,24 @@ function vista_QuienesSomos(puntoMontaje, idioma) {
             </div>`,
             en: `<div class='c-quienes-somos__contenido'>
             <div class='c-quienes-somos__cabecera'>
-            <p class='c-quienes-somos__titulo'>About us?</p>
+            <p class='c-quienes-somos__titulo'>About us</p>
             </div>
             <div class='c-quienes-somos__contenido-interno'>
-            <p class='c-quienes-somos__texto'>We are a company that manages the distribution of food houses.</p>
+            <p class='c-quienes-somos__texto'>We are a fictional company that manages the distribution of food houses.</p>
             <p class='c-quienes-somos__texto'>We collaborate with local food houses to offer the best service.</p>
             <p class='c-quienes-somos__texto'>Eat homemade, eat cheap, eat at your own time, eat Week Food.</p>
             </div>
             <div class='c-quienes-somos__contenido-equipo'>
-            <div class='c-quienes-somos__contenido-equipo-container'><img class='c-quienes-somos__contenido-equipo-imagen' src='/imagenes/yury.jpg'><div class='c-quienes-somos__nombre'>Yuriy</div><div class='c-quienes-somos__texto'>Director of operations</div></div>
-            <div class='c-quienes-somos__contenido-equipo-container'><img class='c-quienes-somos__contenido-equipo-imagen' src='/imagenes/toni.jpg'><div class='c-quienes-somos__nombre'>Toni</div><div class='c-quienes-somos__texto'>Executive Director</div></div>
-            <div class='c-quienes-somos__contenido-equipo-container'><img class='c-quienes-somos__contenido-equipo-imagen' src='/imagenes/jl.jpg'><div class='c-quienes-somos__nombre'>José Luis</div><div class='c-quienes-somos__texto'>Design director</div></div>
+            <div class='c-quienes-somos__contenido-equipo-container'><img class='c-quienes-somos__contenido-equipo-imagen' src='/imagenes/yury.jpg'><div class='c-quienes-somos__nombre'>Yuriy</div><div class='c-quienes-somos__texto'>Chief Operating Officer</div></div>
+            <div class='c-quienes-somos__contenido-equipo-container'><img class='c-quienes-somos__contenido-equipo-imagen' src='/imagenes/toni.jpg'><div class='c-quienes-somos__nombre'>Toni</div><div class='c-quienes-somos__texto'>Chief Executive Officer</div></div>
+            <div class='c-quienes-somos__contenido-equipo-container'><img class='c-quienes-somos__contenido-equipo-imagen' src='/imagenes/jl.jpg'><div class='c-quienes-somos__nombre'>José Luis</div><div class='c-quienes-somos__texto'>Chief Design Officer</div></div>
             </div>
             <div class='c-quienes-somos__servicios'>
             <div class='c-quienes-somos__attCliente'>
-            <p class='c-quienes-somos__titulo-menor'>Contact us.</p>
-            <div class='c-quienes-somos__servicios-container'><div class='c-quienes-somos__servicios-icono'><i class="fas fa-phone"></i></div><div class='c-quienes-somos__texto-icono'>902 - 123- 469</div></div>
+            <p class='c-quienes-somos__titulo-menor'>Contact us</p>
+            <div class='c-quienes-somos__servicios-container'><div class='c-quienes-somos__servicios-icono'><i class="fas fa-phone"></i></div><div class='c-quienes-somos__texto-icono'>(+34)902 - 123- 469</div></div>
             <div class='c-quienes-somos__servicios-container'><div class='c-quienes-somos__servicios-icono'><i class="far fa-envelope"></i></div><div class='c-quienes-somos__texto-icono'>info@weekfood.com</div></div>
-            <div class='c-quienes-somos__servicios-container'><div class='c-quienes-somos__servicios-icono'><i class="fab fa-whatsapp"></i></div><div class='c-quienes-somos__texto-icono'>603 - 123- 469</div></div>
+            <div class='c-quienes-somos__servicios-container'><div class='c-quienes-somos__servicios-icono'><i class="fab fa-whatsapp"></i></div><div class='c-quienes-somos__texto-icono'>(+34)603 - 123- 469</div></div>
             </div>
             <div class='c-quienes-somos__redes'>
             <p class='c-quienes-somos__titulo-menor'>Follow in our social medias!</p>
