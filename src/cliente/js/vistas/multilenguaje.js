8185c13da59007449496ce41e539654225fd1e64
jlsanchezdelgado95 <kokekui17@gmail.com>
2019-01-31 09:01:29 +0100

    Textos destacados a rojo

--- src/cliente/js/vistas/multilenguaje.js
+++ src/cliente/js/vistas/multilenguaje.js
@@ -4,7 +4,7 @@ function vista_QuienesSomos(puntoMontaje, idioma) {
         contenido: {
             es: `<div class='c-quienes-somos__contenido'>
             <div class='c-quienes-somos__cabecera'>
-            <p class='c-quienes-somos__titulo'>Quienes somos</p>
+            <p class='c-quienes-somos__titulo'>Quiénes somos</p>
             </div>
             <div class='c-quienes-somos__contenido-interno'>
             <p class='c-quienes-somos__texto'>Somos una empresa que gestiona el reparto de casas de comida.</p>
@@ -12,13 +12,13 @@ function vista_QuienesSomos(puntoMontaje, idioma) {
             <p class='c-quienes-somos__texto'>Come casero, come barato, come a tu hora, come WeekFood.</p>
             </div>
             <div class='c-quienes-somos__contenido-equipo'>
-            <div class='c-quienes-somos__contenido-equipo-container'><div class='c-quienes-somos__texto'>Yuriy</div><img class='c-quienes-somos__contenido-equipo-imagen' src='/imagenes/yury.jpg'><div class='c-quienes-somos__texto'>Programador Jefe</div></div>
-            <div class='c-quienes-somos__contenido-equipo-container'><div class='c-quienes-somos__texto'>Toni</div><img class='c-quienes-somos__contenido-equipo-imagen' src='/imagenes/toni.jpg'><div class='c-quienes-somos__texto'>Director Ejecutivo</div></div>
-            <div class='c-quienes-somos__contenido-equipo-container'><div class='c-quienes-somos__texto'>José Luis</div><img class='c-quienes-somos__contenido-equipo-imagen' src='/imagenes/jl.jpg'><div class='c-quienes-somos__texto'>Diseñador</div></div>
+            <div class='c-quienes-somos__contenido-equipo-container'><img class='c-quienes-somos__contenido-equipo-imagen' src='/imagenes/yury.jpg'><div class='c-quienes-somos__nombre'>Yuriy</div><div class='c-quienes-somos__texto'>Director de operaciones</div></div>
+            <div class='c-quienes-somos__contenido-equipo-container'><img class='c-quienes-somos__contenido-equipo-imagen' src='/imagenes/toni.jpg'><div class='c-quienes-somos__nombre'>Toni</div><div class='c-quienes-somos__texto'>Director Ejecutivo</div></div>
+            <div class='c-quienes-somos__contenido-equipo-container'><img class='c-quienes-somos__contenido-equipo-imagen' src='/imagenes/jl.jpg'><div class='c-quienes-somos__nombre'>José Luis</div><div class='c-quienes-somos__texto'>Director de diseño</div></div>
             </div>
             <div class='c-quienes-somos__servicios'>
             <div class='c-quienes-somos__attCliente'>
-            <p class='c-quienes-somos__titulo-menor'>No dudes en contactar con nosotros.</p>
+            <p class='c-quienes-somos__titulo-menor'>Contacta con nosotros.</p>
             <div class='c-quienes-somos__servicios-container'><div class='c-quienes-somos__servicios-icono'><i class="fas fa-phone"></i></div><div class='c-quienes-somos__texto-icono'>902 - 123- 469</div></div>
             <div class='c-quienes-somos__servicios-container'><div class='c-quienes-somos__servicios-icono'><i class="far fa-envelope"></i></div><div class='c-quienes-somos__texto-icono'>info@weekfood.com</div></div>
             <div class='c-quienes-somos__servicios-container'><div class='c-quienes-somos__servicios-icono'><i class="fab fa-whatsapp"></i></div><div class='c-quienes-somos__texto-icono'>603 - 123- 469</div></div>
@@ -42,13 +42,13 @@ function vista_QuienesSomos(puntoMontaje, idioma) {
             <p class='c-quienes-somos__texto'>Eat homemade, eat cheap, eat at your own time, eat Week Food.</p>
             </div>
             <div class='c-quienes-somos__contenido-equipo'>
-            <div class='c-quienes-somos__contenido-equipo-container'><div class='c-quienes-somos__texto'>Yuriy</div><img class='c-quienes-somos__contenido-equipo-imagen' src='/imagenes/yury.jpg'><div class='c-quienes-somos__texto'>Chief Programmer</div></div>
-            <div class='c-quienes-somos__contenido-equipo-container'><div class='c-quienes-somos__texto'>Toni</div><img class='c-quienes-somos__contenido-equipo-imagen' src='/imagenes/toni.jpg'><div class='c-quienes-somos__texto'>CEO</div></div>
-            <div class='c-quienes-somos__contenido-equipo-container'><div class='c-quienes-somos__texto'>José Luis</div><img class='c-quienes-somos__contenido-equipo-imagen' src='/imagenes/jl.jpg'><div class='c-quienes-somos__texto'>Designer</div></div>
+            <div class='c-quienes-somos__contenido-equipo-container'><img class='c-quienes-somos__contenido-equipo-imagen' src='/imagenes/yury.jpg'><div class='c-quienes-somos__nombre'>Yuriy</div><div class='c-quienes-somos__texto'>Director of operations</div></div>
+            <div class='c-quienes-somos__contenido-equipo-container'><img class='c-quienes-somos__contenido-equipo-imagen' src='/imagenes/toni.jpg'><div class='c-quienes-somos__nombre'>Toni</div><div class='c-quienes-somos__texto'>Executive Director</div></div>
+            <div class='c-quienes-somos__contenido-equipo-container'><img class='c-quienes-somos__contenido-equipo-imagen' src='/imagenes/jl.jpg'><div class='c-quienes-somos__nombre'>José Luis</div><div class='c-quienes-somos__texto'>Design director</div></div>
             </div>
             <div class='c-quienes-somos__servicios'>
             <div class='c-quienes-somos__attCliente'>
-            <p class='c-quienes-somos__titulo-menor'>If you have some doubt, contact us.</p>
+            <p class='c-quienes-somos__titulo-menor'>Contact us.</p>
             <div class='c-quienes-somos__servicios-container'><div class='c-quienes-somos__servicios-icono'><i class="fas fa-phone"></i></div><div class='c-quienes-somos__texto-icono'>902 - 123- 469</div></div>
             <div class='c-quienes-somos__servicios-container'><div class='c-quienes-somos__servicios-icono'><i class="far fa-envelope"></i></div><div class='c-quienes-somos__texto-icono'>info@weekfood.com</div></div>
             <div class='c-quienes-somos__servicios-container'><div class='c-quienes-somos__servicios-icono'><i class="fab fa-whatsapp"></i></div><div class='c-quienes-somos__texto-icono'>603 - 123- 469</div></div>
