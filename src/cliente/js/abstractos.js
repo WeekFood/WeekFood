483551fabc271a355cd843e94a734f01bba242d9
Hechix <jatonidonet@hotmail.com>
2019-02-01 09:05:19 +0100

    feat(#200) : Vista usuario. PR(#239)

--- src/cliente/js/abstractos.js
+++ src/cliente/js/abstractos.js
@@ -2,12 +2,15 @@ var GLOBAL_VISTAS = {
     error: vista_Error,
     portada: vista_Portada,
     productos: vista_Productos,
+    ofertas: vista_Productos_Ofertas,
+    perfil: vista_Perfil,
     quienesSomos: vista_QuienesSomos,
-    ofertas: vista_Productos_Ofertas
 }
 var GLOBAL_REDIRECCIONES = {
     productos: 'ofertas',
     ofertas: 'ofertas',
+    perfil: 'perfil',
+    yo: 'perfil',
 }
 var GLOBAL_VISTA_ACTUAL = "";
 var GLOBAL_CACHE_JSONS = new CacheJSONs();
