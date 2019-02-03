c145d99d0000270597a1c8439fc55e6b535cb705
yuriyfpmislata <yuriy997+clase@gmail.com>
2019-02-03 12:17:54 +0100

    borrar dist force & build no prod

--- build/build-crud.sh
+++ build/build-crud.sh
@@ -5,7 +5,7 @@ shopt -s globstar # **
 shopt -s extglob # !()
 
 printf "> Borrando ./dist\n"
-rm -r ./dist
+rm -rf ./dist
 printf "> Creando ./dist\n"
 mkdir ./dist
 printf "> Borrando ./dist/crud\n"
@@ -14,7 +14,7 @@ mkdir ./dist/crud
 printf "> Cambiando a ./crud\n"
 cd ./crud
 printf "\t> Ejecutando ng build\n"
-./node_modules/.bin/ng build --prod
+./node_modules/.bin/ng build # --prod
 printf "\t> Copiando todos los recursos generados, menos index.html\n a ../dist\n"
 cp -r ./dist/WeekFood/!(index.html) ../dist/
 printf "\t> Copiando index.html a ../dist/crud\n"
