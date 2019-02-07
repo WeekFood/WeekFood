18423173fb6b2815f569012e69cbbe297c2522e0
yuriyfpmislata <yuriy997+clase@gmail.com>
2019-02-07 10:19:00 +0100

    quitado oninit

--- admin/src/app/views/inicio/inicio.component.ts
+++ admin/src/app/views/inicio/inicio.component.ts
@@ -1,15 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-inicio',
   templateUrl: './inicio.component.html',
   styleUrls: ['./inicio.component.scss']
 })
-export class InicioComponent implements OnInit {
-
+export class InicioComponent {
   constructor() { }
-
-  ngOnInit() {
-  }
-
 }
--- admin/src/app/views/no-encontrado/no-encontrado.component.ts
+++ admin/src/app/views/no-encontrado/no-encontrado.component.ts
@@ -1,15 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-no-encontrado',
   templateUrl: './no-encontrado.component.html',
   styleUrls: ['./no-encontrado.component.scss']
 })
-export class NoEncontradoComponent implements OnInit {
-
+export class NoEncontradoComponent {
   constructor() { }
-
-  ngOnInit() {
-  }
-
 }
