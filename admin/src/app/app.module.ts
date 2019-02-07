e8ff777270f44cb4373a3f00528d84b56fab271b
yuriyfpmislata <yuriy997+clase@gmail.com>
2019-02-07 09:53:52 +0100

    module: quitado httpClient

--- admin/src/app/app.module.ts
+++ admin/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -25,7 +24,6 @@ import { NoEncontradoComponent } from './views/no-encontrado/no-encontrado.compo
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule,
     FormsModule
   ],
   providers: [],
