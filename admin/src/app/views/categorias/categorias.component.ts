5100648da6baa4c864ac50848a40712dc5f1f9c0
yuriyfpmislata <yuriy997+clase@gmail.com>
2019-02-12 22:25:01 +0100

    component inicial tabla

--- admin/src/app/views/categorias/categorias.component.ts
+++ admin/src/app/views/categorias/categorias.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Categoria } from 'src/app/models/Categoria';
+import { CategoriasService } from 'src/app/services/categorias.service';
+
 @Component({
   selector: 'app-categorias',
   templateUrl: './categorias.component.html',
   styleUrls: ['./categorias.component.scss']
 })
 export class CategoriasComponent implements OnInit {
+  categoriaActual: Categoria;
+  categorias: Categoria[];
 
-  constructor() { }
+  constructor(private categoriasService: CategoriasService) { }
 
   ngOnInit() {
+    this.categoriasService.getCategorias()
+      .then((categorias: Categoria[]) => {
+        this.categorias = categorias;
+      });
   }
-
 }
