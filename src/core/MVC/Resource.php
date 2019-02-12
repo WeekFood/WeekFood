4a5faf30d0caf0586924a2d56ae16c4ceddf274e
yuriyfpmislata <yuriy997+clase@gmail.com>
2019-02-12 17:48:05 +0100

    htmlspecialchars
    https://stackoverflow.com/questions/1996122/how-to-prevent-xss-with-html-php

--- src/core/MVC/Resource.php
+++ src/core/MVC/Resource.php
@@ -34,7 +34,7 @@ abstract class Resource {
             $i = 0;
             foreach ($ps->fetchAll(\PDO::FETCH_ASSOC) as $row) {
                 foreach ($row as $key => $value) {
-                    $this->data[$i][$key] = $value;
+                    $this->data[$i][$key] = htmlspecialchars($value, ENT_QUOTES, 'UTF-8');
                 }
                 $i++;
             }
