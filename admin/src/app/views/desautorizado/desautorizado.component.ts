34c5bd6169df2359992fa448db823414cdc29779
Hechix <jatonidonet@hotmail.com>
2019-02-12 21:05:14 +0100

    jquery boton

--- admin/src/app/views/desautorizado/desautorizado.component.ts
+++ admin/src/app/views/desautorizado/desautorizado.component.ts
@@ -1,5 +1,5 @@
 import { Router } from '@angular/router';
-import { Injector,Component, OnInit } from '@angular/core';
+import { Injector, Component, OnInit } from '@angular/core';
 import { environment } from '../../../environments/environment';
 import { AuthService } from 'src/app/services/auth.service';
 import { AuthProviderService } from 'src/app/providers/authprovider.service';
@@ -8,16 +8,13 @@ import { AuthProviderService } from 'src/app/providers/authprovider.service';
   templateUrl: './desautorizado.component.html',
   styleUrls: ['./desautorizado.component.scss']
 })
-export class DesautorizadoComponent implements OnInit {
+export class DesautorizadoComponent {
   constructor(
     private injector: Injector,
     private authService: AuthService,
     private authProvi: AuthProviderService
   ) { }
 
-  ngOnInit() {
-    $(".js-boton-cerrar").on('click', () => { this.cerrarSesion() })
-  }
   //https://stackoverflow.com/questions/39767019/app-initializer-raises-cannot-instantiate-cyclic-dependency-applicationref-w
   public get router(): Router {
     return this.injector.get(Router);
