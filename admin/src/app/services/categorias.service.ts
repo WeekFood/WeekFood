a02644ddcd69adf382b0149aef6fa38e1f663bd5
yuriyfpmislata <yuriy997+clase@gmail.com>
2019-02-12 22:24:13 +0100

    service & getCategorias

---
+++ admin/src/app/services/categorias.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+
+import { environment } from '../../environments/environment';
+import { Categoria } from '../models/Categoria';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CategoriasService {
+  private static API_CATEGORIAS = `http://${window.location.hostname}:${environment.API_PUERTO}/api/productos/categorias`;
+
+  constructor() { }
+
+  getCategorias() {
+    return $.get(CategoriasService.API_CATEGORIAS).then((array: any[]) => {
+      return array.map(categoria => {
+        return new Categoria(
+          categoria.nombre
+        );
+      });
+    });
+  }
+}
