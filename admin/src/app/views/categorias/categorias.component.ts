0c289e078266da646e2d9fa3bcc47f972977928c
yuriyfpmislata <yuriy997+clase@gmail.com>
2019-02-13 11:25:52 +0100

    view: preparar para modal

--- admin/src/app/views/categorias/categorias.component.ts
+++ admin/src/app/views/categorias/categorias.component.ts
@@ -12,12 +12,58 @@ export class CategoriasComponent implements OnInit {
   categoriaActual: Categoria;
   categorias: Categoria[];
 
+  modalAbierto: boolean = false;
+  modoModal: string = '';
+
   constructor(private categoriasService: CategoriasService) { }
 
   ngOnInit() {
+    this.getCategorias();
+  }
+
+  getCategorias() {
     this.categoriasService.getCategorias()
       .then((categorias: Categoria[]) => {
         this.categorias = categorias;
       });
+  }
+
+  abrirModalVer(categoria: Categoria) {
+    this.modalAbierto = true;
+    this.modoModal = 'ver';
+
+    this.categoriaActual = categoria;
+  }
+
+  abrirModalEditar(categoria: Categoria) {
+    this.modalAbierto = true;
+    this.modoModal = 'editar';
+
+    this.categoriaActual = categoria;
+  }
+
+  abrirModalNuevo() {
+    this.modalAbierto = true;
+    this.modoModal = 'crear';
+
+    this.categoriaActual = new Categoria('');
+  }
+
+  abrirModalBorrar(categoria: Categoria) {
+    this.modalAbierto = true;
+    this.modoModal = 'borrar';
+
+    this.categoriaActual = categoria;
+  }
+
+  alCerrarseModal(actualizarse: boolean) {
+    this.modalAbierto = false;
+    this.modoModal = '';
+
+    this.categoriaActual = null;
+
+    if (actualizarse) {
+      this.getCategorias();
+    }
   }
 }
